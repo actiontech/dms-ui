7647038306aa123a6f25e068797c3c4d399260da
wanghong <wanghong@actionsky.com>
2023-10-20 14:49:40 +0800

    [style](System): update EnterpriseFeatureDisplay

--- packages/shared/lib/components/EnterpriseFeatureDisplay/EnterpriseFeatureDisplay.tsx
+++ packages/shared/lib/components/EnterpriseFeatureDisplay/EnterpriseFeatureDisplay.tsx
@@ -8,6 +8,7 @@ import {
   IconBookMark,
   IconCustomService
 } from '../Icons/EnterpriseFeatureDisplay';
+import EmptyBox from '../EmptyBox';
 
 const EnterpriseFeatureDisplay: React.FC<EnterpriseFeatureDisplayProps> = ({
   children,
@@ -18,67 +19,76 @@ const EnterpriseFeatureDisplay: React.FC<EnterpriseFeatureDisplayProps> = ({
   const { t } = useTranslation();
   const renderFeatureDescNode = () => (
     <div className="left">
-      <Typography.Title level={5} className="title">
-        {t('common.enterpriseFeatureDisplay.featureDescription')}
-      </Typography.Title>
+      <EmptyBox if={!isConfigPage}>
+        <Typography.Title level={5} className="title">
+          {t('common.enterpriseFeatureDisplay.featureDescription')}
+        </Typography.Title>
+      </EmptyBox>
+
       {eeFeatureDescription}
     </div>
   );
+
+  const renderBookMarkCard = () => (
+    <Card className="ce-card-info" hoverable>
+      <Icon component={IconBookMark} className="bg-icon" />
+      <div className="info-cont-wrapper">
+        <div className="title">Action DMS</div>
+        <p className="cont-tip">
+          {t('common.enterpriseFeatureDisplay.compareLink')}
+        </p>
+        <div className="button-group-wrapper">
+          <a
+            target="_blank"
+            href="https://actiontech.github.io/sqle-docs/docs/support/compare"
+            rel="noreferrer"
+          >
+            <BasicButton type="primary">
+              {t('common.enterpriseFeatureDisplay.userBook')}
+            </BasicButton>
+          </a>
+        </div>
+      </div>
+    </Card>
+  );
+
+  const renderCustomServiceCard = () => (
+    <Card className="ce-card-info" hoverable>
+      <Icon component={IconCustomService} className="bg-icon" />
+      <div className="info-cont-wrapper">
+        <div className="title">Action DMS</div>
+        <p className="cont-tip">
+          {t('common.enterpriseFeatureDisplay.businessLink', {
+            featureName
+          })}
+        </p>
+        <Space size={[12, 0]} className="button-group-wrapper">
+          <a
+            target="_blank"
+            href="https://github.com/actiontech/sqle"
+            rel="noreferrer"
+          >
+            <BasicButton type="primary">SQLE Github</BasicButton>
+          </a>
+
+          <a target="_blank" href="https://www.actionsky.com/" rel="noreferrer">
+            <BasicButton type="primary">
+              {t('common.enterpriseFeatureDisplay.linkUs')}
+            </BasicButton>
+          </a>
+        </Space>
+      </div>
+    </Card>
+  );
   const renderAdditionalAttentionNode = () => (
     <div className="right">
-      <Typography.Title level={5} className="title">
-        {t('common.enterpriseFeatureDisplay.additionalAttention')}
-      </Typography.Title>
-      <Card className="ce-card-info" hoverable>
-        <Icon component={IconBookMark} className="bg-icon" />
-        <div className="info-cont-wrapper">
-          <div className="title">Action DMS</div>
-          <p className="cont-tip">
-            {t('common.enterpriseFeatureDisplay.compareLink')}
-          </p>
-          <div className="button-group-wrapper">
-            <a
-              target="_blank"
-              href="https://actiontech.github.io/sqle-docs/docs/support/compare"
-              rel="noreferrer"
-            >
-              <BasicButton type="primary">
-                {t('common.enterpriseFeatureDisplay.userBook')}
-              </BasicButton>
-            </a>
-          </div>
-        </div>
-      </Card>
-      <Card className="ce-card-info" hoverable>
-        <Icon component={IconCustomService} className="bg-icon" />
-        <div className="info-cont-wrapper">
-          <div className="title">Action DMS</div>
-          <p className="cont-tip">
-            {t('common.enterpriseFeatureDisplay.businessLink', {
-              featureName
-            })}
-          </p>
-          <Space size={[12, 0]} className="button-group-wrapper">
-            <a
-              target="_blank"
-              href="https://github.com/actiontech/sqle"
-              rel="noreferrer"
-            >
-              <BasicButton type="primary">SQLE Github</BasicButton>
-            </a>
-
-            <a
-              target="_blank"
-              href="https://www.actionsky.com/"
-              rel="noreferrer"
-            >
-              <BasicButton type="primary">
-                {t('common.enterpriseFeatureDisplay.linkUs')}
-              </BasicButton>
-            </a>
-          </Space>
-        </div>
-      </Card>
+      <EmptyBox if={!isConfigPage}>
+        <Typography.Title level={5} className="title">
+          {t('common.enterpriseFeatureDisplay.additionalAttention')}
+        </Typography.Title>
+      </EmptyBox>
+      {renderCustomServiceCard()}
+      {renderBookMarkCard()}
     </div>
   );
 
