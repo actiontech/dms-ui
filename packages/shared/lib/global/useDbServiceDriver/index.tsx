d57242456f8db8887232783656a18a416bb3a662
wanghong <wanghong@actionsky.com>
2023-11-01 15:51:48 +0800

    chore: supply useDbServiceDriver

---
+++ packages/shared/lib/global/useDbServiceDriver/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback } from 'react';
+import { useBoolean } from 'ahooks';
+import useCurrentProject from '../useCurrentProject';
+import dms from '../../api/base/service/dms';
+import { ResponseCode } from '../../enum';
+import { IDatabaseDriverOption } from '../../api/base/service/common';
+
+const useDbServiceDriver = () => {
+  const [driverNameList, setDriverNameList] = React.useState<string[]>([]);
+  const [driverMeta, setDriverMeta] = React.useState<IDatabaseDriverOption[]>(
+    []
+  );
+  const [loading, { setTrue: startLoading, setFalse: loadFinish }] =
+    useBoolean();
+
+  const { projectID } = useCurrentProject();
+
+  const updateDriverNameList = React.useCallback(() => {
+    startLoading();
+    dms
+      .ListDBServiceDriverOption({
+        project_uid: projectID
+      })
+      .then((res) => {
+        if (res.data.code === ResponseCode.SUCCESS) {
+          setDriverMeta(res.data.data ?? []);
+          setDriverNameList(res.data.data?.map((v) => v.db_type ?? '') ?? []);
+        } else {
+          setDriverNameList([]);
+          setDriverMeta([]);
+        }
+      })
+      .catch(() => {
+        setDriverMeta([]);
+        setDriverNameList([]);
+      })
+      .finally(() => {
+        loadFinish();
+      });
+  }, [loadFinish, startLoading, projectID]);
+
+  const getLogoUrlByDbType = useCallback(
+    (dbType: string) => {
+      if (!dbType) return '';
+
+      return (
+        driverMeta.find((driver) => dbType === driver?.db_type)?.logo_path ?? ''
+      );
+    },
+    [driverMeta]
+  );
+
+  return {
+    driverNameList,
+    loading,
+    updateDriverNameList,
+    driverMeta,
+    getLogoUrlByDbType
+  };
+};
+export default useDbServiceDriver;
