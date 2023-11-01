6d41c0b256fe2edcc6d75febd378f5cc307e69ae
wanghong <wanghong@actionsky.com>
2023-11-01 16:54:35 +0800

    fix: change way to get bdType LogoUrl

--- packages/base/src/hooks/useTaskSource/index.tsx
+++ packages/base/src/hooks/useTaskSource/index.tsx
@@ -3,7 +3,10 @@ import { useBoolean } from 'ahooks';
 import { Select } from 'antd5';
 import { ResponseCode } from '@actiontech/shared/lib/enum';
 import { IDatabaseSource } from '@actiontech/shared/lib/api/base/service/common';
-import { useCurrentProject } from '@actiontech/shared/lib/global';
+import {
+  useCurrentProject,
+  useDbServiceDriver
+} from '@actiontech/shared/lib/global';
 import { DatabaseTypeLogo } from '@actiontech/shared';
 import dms from '@actiontech/shared/lib/api/base/service/dms';
 
@@ -14,6 +17,7 @@ const useTaskSource = () => {
   const [loading, { setTrue, setFalse }] = useBoolean();
 
   const { projectID } = useCurrentProject();
+  const { getLogoUrlByDbType } = useDbServiceDriver();
 
   const updateTaskSourceList = React.useCallback(() => {
     setTrue();
@@ -51,12 +55,15 @@ const useTaskSource = () => {
       return dbTypes.map((type) => {
         return (
           <Select.Option key={type} value={type ?? ''}>
-            <DatabaseTypeLogo dbType={type} logoUrl="" />
+            <DatabaseTypeLogo
+              dbType={type}
+              logoUrl={getLogoUrlByDbType(type ?? '')}
+            />
           </Select.Option>
         );
       });
     },
-    [taskSourceList]
+    [getLogoUrlByDbType, taskSourceList]
   );
 
   return {
