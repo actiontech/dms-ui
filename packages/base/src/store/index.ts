3932661f00c6a90d5d9a7c1e95c4a7e55651bef9
lizhensheng <lizhensheng@actionsky.com>
2023-11-10 17:57:49 +0800

    [feature]: Add findDuplicateKeys to check store

--- packages/base/src/store/index.ts
+++ packages/base/src/store/index.ts
@@ -2,16 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import { baseStoreData } from './base';
 import { SQLEStoreData } from 'sqle/src/store';
 import { diagnosisStoreData } from 'diagnosis/src/store';
+import { findDuplicateKeys } from '../utils/findDuplicateKeys';
 
 // IFTRUE_isDebug
-const getKeysLen = (obj: Record<string, any>) => Object.keys(obj).length;
-if (
-  getKeysLen({ ...baseStoreData, ...SQLEStoreData, ...diagnosisStoreData }) !==
-  getKeysLen(baseStoreData) +
-    getKeysLen(SQLEStoreData) +
-    getKeysLen(diagnosisStoreData)
-) {
-  throw new Error('Redux store error: The same key exists');
+const dupKeys = findDuplicateKeys([
+  baseStoreData,
+  SQLEStoreData,
+  diagnosisStoreData
+]);
+if (dupKeys.length > 0) {
+  throw new Error(
+    `Redux store error: The same key exists: ${dupKeys.toString()}`
+  );
 }
 // FITRUE_isDebug
 
