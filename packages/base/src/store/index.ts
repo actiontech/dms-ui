2b1eb2dfc57b95c81b9c4a40b8da7da21cea0813
lizhensheng <lizhensheng@actionsky.com>
2023-11-10 17:31:14 +0800

    [chore]: Add dev environment to check whether the same redux key exists

--- packages/base/src/store/index.ts
+++ packages/base/src/store/index.ts
@@ -2,6 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import { baseStoreData } from './base';
 import { SQLEStoreData } from 'sqle/src/store';
 
+// IFTRUE_isDebug
+const getKeysLen = (obj: Record<string, any>) => Object.keys(obj).length;
+if (
+  getKeysLen({ ...baseStoreData, ...SQLEStoreData }) !==
+  getKeysLen(baseStoreData) + getKeysLen(SQLEStoreData)
+) {
+  throw new Error('Redux store error: The same key exists');
+}
+// FITRUE_isDebug
+
 const store = configureStore({
   reducer: {
     ...baseStoreData,
