44a617528ad00b0b528098d320b26df9f551d290
lizhensheng <lizhensheng@actionsky.com>
2023-11-23 11:16:00 +0800

    [chore]: fix style lint

--- packages/shared/lib/components/BasicDatePicker/style.ts
+++ packages/shared/lib/components/BasicDatePicker/style.ts
@@ -117,6 +117,7 @@ export const BasicDatePickerFieldStyleWrapper = styled(
     width: 100%;
     padding-left: 40px !important;
     border-radius: 4px !important;
+
     &:hover:not(:focus):not(.ant-picker-status-error):not(
         .ant-picker-disabled
       ):not(.ant-picker-focused):not(.ant-picker-borderless) {
