cf639c3cef699a59310305ca0d7966dd5799ff57
wanghong <wanghong@actionsky.com>
2023-11-24 08:49:37 +0800

    fix: remove useless style wrapper

--- packages/sqle/src/page/SqlManagement/component/SQLEEIndex/style.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from '@mui/material/styles';
-
-export const SqlManagementListStyleWrapper = styled('section')`
-  .sqle-management-list-table-remark-column {
-    max-width: 400px;
-  }
-`;
