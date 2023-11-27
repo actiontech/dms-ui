174a10a76e5a6f5c29b4876d73b46ddf670ef931
xinbinyan <404928130@qq.com>
2023-11-27 17:45:36 +0800

    update unit test error for some file

--- packages/shared/lib/components/TestDatabaseConnectButton/index.test.tsx
+++ packages/shared/lib/components/TestDatabaseConnectButton/index.test.tsx
@@ -1,66 +1,63 @@
 import TestDatabaseConnectButton from '.';
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, screen } from '@testing-library/react';
+import { renderWithTheme } from '../../testUtil/customRender';
+import { TestDatabaseConnectButtonProps } from './index.type';
 
 describe('TestDatabaseConnectButton', () => {
   const buttonText = '测试数据库连通性';
 
+  const customRender = (params: TestDatabaseConnectButtonProps) => {
+    return renderWithTheme(<TestDatabaseConnectButton {...params} />);
+  };
+
   describe('uncontrolled mode', () => {
     test('should render message by initHide', () => {
-      const { container, rerender } = render(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          loading={true}
-          connectAble={true}
-        />
-      );
+      const { container } = customRender({
+        onClickTestButton: jest.fn,
+        loading: true,
+        connectAble: true
+      });
       expect(container).toMatchSnapshot();
 
-      rerender(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          loading={false}
-          connectAble={true}
-        />
-      );
+      const { container: container1 } = customRender({
+        onClickTestButton: jest.fn,
+        loading: false,
+        connectAble: true
+      });
 
-      expect(container).toMatchSnapshot();
+      expect(container1).toMatchSnapshot();
     });
 
     test('should show connect result when click the test button', () => {
       const handleClickButton = jest.fn();
-      const { container, rerender } = render(
-        <TestDatabaseConnectButton
-          onClickTestButton={handleClickButton}
-          loading={true}
-          connectAble={true}
-        />
-      );
+      customRender({
+        onClickTestButton: handleClickButton,
+        loading: true,
+        connectAble: true
+      });
       fireEvent.click(screen.getByText(buttonText));
       expect(handleClickButton).not.toBeCalled();
 
-      rerender(
-        <TestDatabaseConnectButton
-          onClickTestButton={handleClickButton}
-          loading={false}
-          connectAble={true}
-        />
-      );
-      expect(container).toMatchSnapshot();
-      fireEvent.click(screen.getByText(buttonText));
+      const { container: container1 } = customRender({
+        onClickTestButton: handleClickButton,
+        loading: false,
+        connectAble: true
+      });
+      expect(container1).toMatchSnapshot();
+      fireEvent.click(screen.getAllByText(buttonText)[1]);
       expect(handleClickButton).toBeCalledTimes(1);
-      expect(container).toMatchSnapshot();
+      expect(container1).toMatchSnapshot();
 
       handleClickButton.mockClear();
       cleanup();
 
-      const { container: disableContainer } = render(
-        <TestDatabaseConnectButton
-          onClickTestButton={handleClickButton}
-          loading={false}
-          connectAble={false}
-          connectDisableReason="disable reason"
-        />
-      );
+      const { container: disableContainer } = customRender({
+        onClickTestButton: handleClickButton,
+        loading: false,
+        connectAble: true,
+        connectDisableReason: 'disable reason'
+      });
+
       expect(disableContainer).toMatchSnapshot();
       fireEvent.click(screen.getByText(buttonText));
       expect(handleClickButton).toBeCalledTimes(1);
@@ -70,67 +67,53 @@ describe('TestDatabaseConnectButton', () => {
 
   describe('controlled mode', () => {
     test('should render message by initHide', () => {
-      const { container, rerender } = render(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          initHide={true}
-          loading={false}
-          connectAble={false}
-        />
-      );
+      const { container } = customRender({
+        onClickTestButton: jest.fn,
+        initHide: true,
+        loading: false,
+        connectAble: false
+      });
       expect(container).toMatchSnapshot();
 
-      rerender(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          initHide={true}
-          loading={true}
-          connectAble={false}
-        />
-      );
+      const { container: container1 } = customRender({
+        onClickTestButton: jest.fn,
+        initHide: true,
+        loading: true,
+        connectAble: false
+      });
+      expect(container1).toMatchSnapshot();
 
-      expect(container).toMatchSnapshot();
+      const { container: container2 } = customRender({
+        onClickTestButton: jest.fn,
+        initHide: false,
+        loading: true,
+        connectAble: false,
+        connectDisableReason: 'disable reason'
+      });
+      expect(container2).toMatchSnapshot();
 
-      rerender(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          initHide={false}
-          loading={true}
-          connectAble={false}
-          connectDisableReason="disable reason"
-        />
-      );
+      const { container: container3 } = customRender({
+        onClickTestButton: jest.fn,
+        initHide: false,
+        loading: false,
+        connectAble: true,
+        connectDisableReason: 'disable reason'
+      });
+      expect(container3).toMatchSnapshot();
 
-      expect(container).toMatchSnapshot();
-
-      rerender(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          initHide={false}
-          loading={false}
-          connectAble={true}
-          connectDisableReason="disable reason"
-        />
-      );
-
-      expect(container).toMatchSnapshot();
-
-      rerender(
-        <TestDatabaseConnectButton
-          onClickTestButton={jest.fn}
-          initHide={false}
-          loading={false}
-          connectAble={false}
-          connectDisableReason="disable reason"
-        />
-      );
-
-      expect(container).toMatchSnapshot();
+      const { container: container4 } = customRender({
+        onClickTestButton: jest.fn,
+        initHide: false,
+        loading: false,
+        connectAble: false,
+        connectDisableReason: 'disable reason'
+      });
+      expect(container4).toMatchSnapshot();
     });
 
     test('should call onClickTestButton when click the test button', () => {
       const handleClickTestButton = jest.fn();
-      const { container } = render(
+      const { container } = renderWithTheme(
         <TestDatabaseConnectButton
           onClickTestButton={handleClickTestButton}
           initHide={true}
