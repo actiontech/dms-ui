0b339e55dfca9767880b770c280ddd56c802a12b
xinbinyan <404928130@qq.com>
2023-11-27 18:10:41 +0800

    case useDbService

--- packages/base/src/testUtils/mockApi/dbServices/index.ts
+++ packages/base/src/testUtils/mockApi/dbServices/index.ts
@@ -19,12 +19,7 @@ class MockDbServicesApi implements MockSpyApy {
 
   public ListDBServices() {
     const spy = jest.spyOn(dms, 'ListDBServices');
-    spy.mockImplementation(() =>
-      createSpySuccessResponse({
-        total: dbServices.length,
-        db_services: dbServices
-      })
-    );
+    spy.mockImplementation(() => createSpySuccessResponse(dbServices));
     return spy;
   }
 
