8c9ef786c6b7f8a82e4f51856e044abe54fcab85
xinbinyan <404928130@qq.com>
2023-11-30 13:35:34 +0800

    case BasicTag

--- packages/shared/lib/components/BasicTag/index.tsx
+++ packages/shared/lib/components/BasicTag/index.tsx
@@ -3,9 +3,17 @@ import { BasicTagStyleWrapper } from './style';
 import classNames from 'classnames';
 import { BasicTagColor } from '../../theme/theme.type';
 
-const BasicTag: React.FC<
-  TagProps & { size?: 'small' | 'large' | 'default'; color?: BasicTagColor }
-> = ({ className, size = 'default', color = 'default', ...props }) => {
+export type IBasicTag = TagProps & {
+  size?: 'small' | 'large' | 'default';
+  color?: BasicTagColor;
+};
+
+const BasicTag: React.FC<IBasicTag> = ({
+  className,
+  size = 'default',
+  color = 'default',
+  ...props
+}) => {
   return (
     <BasicTagStyleWrapper
       {...props}
