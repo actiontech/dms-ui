9297ffa44664668ddf321b6f360b78d4ddaadc17
xinbinyan <404928130@qq.com>
2023-12-05 14:15:40 +0800

    case ActiontechTable

---
+++ packages/shared/lib/components/ActiontechTable/index.test.tsx
@@ -0,0 +1,175 @@
+import { getBySelector } from '../../testUtil/customQuery';
+import { fireEvent, act, cleanup, screen } from '@testing-library/react';
+import { renderWithTheme } from '../../testUtil/customRender';
+
+import ActiontechTable from './Table';
+import { ActiontechTableProps, TypeFilterElement } from './index.type';
+
+describe('lib/ActiontechTable', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllTimers();
+    cleanup();
+  });
+
+  const customRender = (params: ActiontechTableProps) => {
+    return renderWithTheme(<ActiontechTable {...params} />);
+  };
+
+  describe('-normal table', () => {
+    it('render normal table is empty', () => {
+      const { baseElement } = customRender({
+        dataSource: [],
+        pagination: false
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render normal table is loading', () => {
+      const { baseElement } = customRender({
+        dataSource: [],
+        loading: true,
+        pagination: false
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render normal table is error info', () => {
+      const { baseElement } = customRender({
+        dataSource: [],
+        loading: false,
+        errorMessage: 'this is error message'
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render table with no pagination', () => {
+      const { baseElement } = customRender({
+        pagination: false,
+        dataSource: [
+          { name: 'a', age: 11 },
+          { name: 'b', age: 18 },
+          { name: 'c', age: 19 }
+        ],
+        rowKey: 'name',
+        columns: [
+          {
+            dataIndex: 'a',
+            title: '名称'
+          },
+          {
+            dataIndex: 'b',
+            title: '年龄'
+          }
+        ]
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render table with pagination', async () => {
+      const totalNum = 50;
+      const mockData = [];
+      for (let i = 0; i <= totalNum; i++) {
+        mockData.push({
+          name: `name${i + 1}`,
+          age: i + 1
+        });
+      }
+      const onChangeFn = jest.fn();
+      const { baseElement } = customRender({
+        rowKey: 'name',
+        dataSource: mockData,
+        pagination: { total: totalNum },
+        onChange: onChangeFn
+      });
+      expect(baseElement).toMatchSnapshot();
+      expect(screen.getByText('共 50 条数据')).toBeInTheDocument();
+
+      const pageIndex2 = getBySelector('.ant-pagination-item-2', baseElement);
+      await act(async () => {
+        fireEvent.click(pageIndex2);
+        await jest.advanceTimersByTime(300);
+      });
+      expect(onChangeFn).toBeCalledTimes(1);
+      expect(baseElement).toMatchSnapshot();
+    });
+  });
+
+  describe('-composite table', () => {
+    const totalNum = 50;
+    const mockData: { name: string; age: number }[] = [];
+    for (let i = 0; i <= totalNum; i++) {
+      mockData.push({
+        name: `name${i + 1}`,
+        age: i + 1
+      });
+    }
+
+    it('render table has tool', () => {
+      const { baseElement } = customRender({
+        dataSource: mockData,
+        rowKey: 'name',
+        setting: {
+          tableName: 'test_list',
+          username: 'admin'
+        },
+        pagination: {
+          total: totalNum
+        }
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render table has toolbar children', () => {
+      const { baseElement } = customRender({
+        dataSource: mockData,
+        rowKey: 'name',
+        toolbar: {
+          setting: {
+            tableName: 'test_list',
+            username: 'admin'
+          },
+          children: <>tool children node</>
+        },
+        pagination: {
+          total: totalNum
+        }
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render table with filter', () => {
+      const { baseElement } = customRender({
+        dataSource: mockData,
+        rowKey: 'name',
+        pagination: {
+          total: totalNum
+        },
+        filterContainerProps: {
+          updateTableFilterInfo: jest.fn(),
+          disabled: false,
+          filterContainerMeta: [
+            {
+              dataIndex: 'demo1',
+              filterCustomType: 'select' as TypeFilterElement,
+              filterKey: 'a',
+              filterLabel: '下拉'
+            }
+          ]
+        }
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('render table with class name', () => {
+      const { baseElement } = customRender({
+        className: 'custom-class-table-test'
+      });
+      expect(baseElement).toMatchSnapshot();
+    });
+  });
+});
