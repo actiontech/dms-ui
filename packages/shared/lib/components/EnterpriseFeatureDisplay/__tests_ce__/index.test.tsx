713c99322c2ea711a53121522e22b0200ae4629d
lizhensheng <lizhensheng@actionsky.com>
2023-12-05 10:15:44 +0800

    [test]: test github action

--- packages/shared/lib/components/EnterpriseFeatureDisplay/__tests_ce__/index.test.tsx
+++ packages/shared/lib/components/EnterpriseFeatureDisplay/__tests_ce__/index.test.tsx
@@ -4,11 +4,24 @@ import { renderWithTheme } from '../../../testUtil/customRender';
 const children = <>ee version display</>;
 
 describe('test ce EnterpriseFeatureDisplay', () => {
-  test('should match snapshot', () => {
+  it('should match snapshot', () => {
     const { container } = renderWithTheme(
       <EnterpriseFeatureDisplay
         featureName="featureName"
         eeFeatureDescription="eeFeatureDescription"
+      >
+        {children}
+      </EnterpriseFeatureDisplay>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot when isConfigPage is equal true', () => {
+    const { container } = renderWithTheme(
+      <EnterpriseFeatureDisplay
+        featureName="featureName"
+        eeFeatureDescription="eeFeatureDescription"
+        isConfigPage
       >
         {children}
       </EnterpriseFeatureDisplay>
