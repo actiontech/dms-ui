678e946f0c8b5d583c1d60537bcbd7c226302ed9
sunjinkang <2060037942@qq.com>
2023-12-06 18:03:34 +0800

    [test]: add test for monitor source config

---
+++ packages/diagnosis/src/testUtils/mockApi/login/index.ts
@@ -0,0 +1,26 @@
+import {
+  MockSpyApy,
+  createSpySuccessResponse
+} from '@actiontech/shared/lib/testUtil/mockApi';
+import auth from '../../../api/auth';
+
+class MockLoginApi implements MockSpyApy {
+  public mockAllApi(): void {
+    this.login();
+  }
+
+  public login() {
+    const spy = jest.spyOn(auth, 'V1Login');
+    spy.mockImplementation(() =>
+      createSpySuccessResponse({
+        code: 0,
+        message: 'ok',
+        token: 'login',
+        user_id: 1
+      })
+    );
+    return spy;
+  }
+}
+
+export default new MockLoginApi();
--- packages/diagnosis/src/testUtils/mockApi/monitorSourceConfig/data.ts
+++ packages/diagnosis/src/testUtils/mockApi/monitorSourceConfig/data.ts
@@ -1,4 +1,7 @@
-import { ViewServerReplyStatusEnum } from '../../../api/common.enum';
+import {
+  ViewDatabaseReplyStatusEnum,
+  ViewServerReplyStatusEnum
+} from '../../../api/common.enum';
 
 export const searchServerMonitorListData = [
   {
@@ -37,4 +40,63 @@ export const serverMonitorListData = [
   }
 ];
 
-export const databaseMonitorListData = [];
+export const databaseMonitorListData = [
+  {
+    created_at: '2023-11-03T10:02:51.132+08:00',
+    monitor_type: 'MySQL',
+    host: '172.20.134.1',
+    monitor_name: 'first',
+    port: 22,
+    status: ViewDatabaseReplyStatusEnum.healthy,
+    id: '1731574922989273088',
+    username: 'root'
+  },
+  {
+    created_at: '2023-11-03T10:02:51.132+08:00',
+    monitor_type: 'MySQL',
+    host: '172.20.134.2',
+    monitor_name: 'second',
+    port: 22,
+    id: '1731574922989273089',
+    username: 'root',
+    status: ViewDatabaseReplyStatusEnum.unhealthy
+  },
+  {
+    created_at: '2023-11-03T10:02:51.132+08:00',
+    monitor_type: 'MySQL',
+    host: '172.20.134.3',
+    monitor_name: 'third',
+    port: 22,
+    id: '1731574922989273090',
+    username: 'root',
+    status: ViewDatabaseReplyStatusEnum.unknown
+  }
+];
+
+export const monitorRoutineListData = [
+  {
+    desc: 'mysql status 监控',
+    enable: true,
+    id: '1731574922997661696',
+    interval: 15,
+    monitor_id: 1,
+    monitor_name: 'mysql_status',
+    via: 'show global status'
+  },
+  {
+    desc: 'mysql 内存监控',
+    enable: false,
+    id: '1731574922997661697',
+    interval: 15,
+    monitor_id: 2,
+    monitor_name: 'mysql_mem',
+    via: '/proc/{mysql_pid}/status'
+  }
+];
+
+export const monitorRoutineMetricsListData = [
+  {
+    desc: 'mysql状态值Com_*/mysql status value Com_',
+    metric_key: 'mysql:server_status:commands'
+  }
+];
