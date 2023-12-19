4ca11e2e144e8f0e4af0812f54dc767a3283390c
xinbinyan <404928130@qq.com>
2023-12-19 16:30:16 +0800

    case login

--- packages/base/src/page/Login/index.tsx
+++ packages/base/src/page/Login/index.tsx
@@ -60,7 +60,6 @@ const Login = () => {
               token
             })
           );
-
           const params = new URLSearchParams(location.search);
           const target = params.get(DMS_REDIRECT_KEY_PARAMS_NAME);
           if (target) {
@@ -84,7 +83,9 @@ const Login = () => {
   };
 
   const { run: getOauth2Tips, data: oauthConfig } = useRequest(
-    () => dms.GetOauth2Tips().then((res) => res.data?.data ?? {}),
+    () => {
+      return dms.GetOauth2Tips().then((res) => res.data?.data ?? {});
+    },
     {
       manual: true
     }
