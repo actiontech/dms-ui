bf9bd8abb93582d30a9c0b9fbd91d6e98d413bdb
zzyangh <799463087@qq.com>
2023-12-27 11:06:00 +0800

    fix: review issue

--- packages/base/src/page/Nav/SideMenu/UserMenu/Modal/VersionModal/index.sqle.ce.tsx
+++ packages/base/src/page/Nav/SideMenu/UserMenu/Modal/VersionModal/index.sqle.ce.tsx
@@ -1,6 +1,6 @@
 import VersionComparison from '@actiontech/shared/lib/components/EnterpriseFeatureDisplay/components/VersionComparison';
-import SQLEContent from '../SQLEContent';
-import BasicVersionModal from '../BasicVersionModal';
+import SQLEVersionContent from './SQLEVersionContent';
+import BasicVersionModal from './BasicVersionModal';
 
 const SQLModalCE: React.FC<{
   open: boolean;
@@ -8,9 +8,9 @@ const SQLModalCE: React.FC<{
 }> = ({ open, setVersionModalClose }) => {
   return (
     <BasicVersionModal open={open} setVersionModalClose={setVersionModalClose}>
-      <SQLEContent open={open}>
+      <SQLEVersionContent open={open}>
         <VersionComparison />
-      </SQLEContent>
+      </SQLEVersionContent>
     </BasicVersionModal>
   );
 };
