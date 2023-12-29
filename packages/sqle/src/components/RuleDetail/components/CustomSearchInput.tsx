203c33f8c4af56cb08fc1cb7dcedee00d0956678
zzyangh <799463087@qq.com>
2023-12-29 16:43:44 +0800

    [feature](RuleTemplate): Fuzzy queries when adding templates

--- packages/sqle/src/components/RuleDetail/components/CustomSearchInput.tsx
+++ packages/sqle/src/components/RuleDetail/components/CustomSearchInput.tsx
@@ -1,24 +1,42 @@
 import { ICustomInputProps } from '@actiontech/shared/lib/components/CustomInput';
 import { RuleDetailCustomSearchInputStyleWrapper } from '../style';
 import { IconSearch } from '@actiontech/shared/lib/Icon';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import EventEmitter from '../../../utils/EventEmitter';
+import EmitterKey from '../../../data/EmitterKey';
 
 const CustomSearchInput: React.FC<ICustomInputProps> = ({
   onCustomPressEnter,
   ...otherProps
 }) => {
   const [value, setValue] = useState<string>('');
+
+  useEffect(() => {
+    const { unsubscribe } = EventEmitter.subscribe(
+      EmitterKey.Search_Rule_Template_Rule_Clear_Value,
+      () => {
+        setValue('');
+        EventEmitter.emit(EmitterKey.Search_Rule_Template_Rule_Select_List, '');
+      }
+    );
+
+    return unsubscribe;
+  }, []);
+
   return (
     <RuleDetailCustomSearchInputStyleWrapper
+      size="small"
       className="custom-search-input"
-      onCustomPressEnter={onCustomPressEnter}
+      onPressEnter={(e) => {
+        onCustomPressEnter(e.target.value ?? '');
+      }}
       onChange={(e) => {
         setValue(e.target.value);
       }}
       suffix={
         <IconSearch
           className="pointer"
-          onClick={() => onCustomPressEnter?.(value)}
+          onClick={() => onCustomPressEnter?.(value as string)}
         />
       }
       value={value}
