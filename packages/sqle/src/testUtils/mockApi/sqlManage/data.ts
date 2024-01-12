88469ee72b252af8b79ddad7ba20414e9e07b23a
sunjinkang <2060037942@qq.com>
2024-01-12 19:19:08 +0800

    [test]: add test for sql management hooks

---
+++ packages/sqle/src/testUtils/mockApi/sqlManage/data.ts
@@ -0,0 +1,31 @@
+export const ruleTipsData = [
+  {
+    db_type: 'MySQL',
+    rule: [
+      {
+        desc: '用于测试',
+        rule_name: 'test'
+      }
+    ]
+  },
+  {
+    rule: [
+      {
+        desc: '用于测试',
+        rule_name: 'test'
+      }
+    ]
+  },
+  {
+    db_type: 'MySQL'
+  }
+];
+
+export const sqlManageListData = {
+  data: [{}],
+  sql_manage_bad_num: 1,
+  sql_manage_optimized_num: 1,
+  sql_manage_total_num: 2
+};
+
+export const exportSqlManageData = 'test';
