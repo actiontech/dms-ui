25bcf6c1490bf97b4ffff0783a744b61eb0012f5
zzyangh <799463087@qq.com>
2024-01-15 16:09:52 +0800

    fix: i18n issue

--- packages/sqle/src/page/SqlAudit/Create/SQLStatementForm/SqlUploadType.tsx
+++ packages/sqle/src/page/SqlAudit/Create/SQLStatementForm/SqlUploadType.tsx
@@ -1,51 +1,15 @@
-import { SQLUploadTypeKeys, TypeUploadKeys } from './index.type';
-import {
-  IconOrderFileUpload,
-  IconOrderSQLUpload
-} from '../../../../icon/Order';
-import { SettingOutlined } from '@ant-design/icons';
+import { SQLUploadTypeKeys } from './index.type';
 import { UploadTypeStyleWrapper } from '../../../Order/SQLStatementForm/style';
 import UploadTypeItem from '../../../Order/SQLStatementForm/UploadTypeItem';
 import { UploadTypeEnum } from '../SQLInfoForm/index.type';
-import { useTranslation } from 'react-i18next';
 import { useContext } from 'react';
 import { FormSubmitStatusContext } from '..';
-import { I18nKey } from '../../../../locale';
-
-const uploadItem: {
-  [key in TypeUploadKeys]: {
-    icon: React.ReactNode;
-    title: I18nKey;
-  };
-} = {
-  sql: {
-    icon: <IconOrderSQLUpload />,
-    title: 'sqlAudit.create.sqlInfo.uploadTypeEnum.sql'
-  },
-  git: {
-    icon: <SettingOutlined style={{ color: '#c3c6cd' }} />,
-    title: 'sqlAudit.create.sqlInfo.uploadTypeEnum.git'
-  },
-  sqlFile: {
-    icon: <IconOrderFileUpload />,
-    title: 'sqlAudit.create.sqlInfo.uploadTypeEnum.sqlFile'
-  },
-  mybatisFile: {
-    icon: <IconOrderFileUpload />,
-    title: 'sqlAudit.create.sqlInfo.uploadTypeEnum.mybatisFile'
-  },
-  zipFile: {
-    icon: <IconOrderFileUpload />,
-    title: 'sqlAudit.create.sqlInfo.uploadTypeEnum.zipFile'
-  }
-};
+import { uploadItem } from './index.data';
 
 const SqlUploadType: React.FC<{
   value?: UploadTypeEnum;
   onChange?: (v: UploadTypeEnum) => void;
 }> = ({ value, onChange }) => {
-  const { t } = useTranslation();
-
   const submitLoading = useContext(FormSubmitStatusContext);
 
   return (
@@ -62,7 +26,7 @@ const SqlUploadType: React.FC<{
             active={value === UploadTypeEnum[type]}
           >
             {uploadItem[type].icon}
-            <span className="text">{t(uploadItem[type].title)}</span>
+            <span className="text">{uploadItem[type].title}</span>
           </UploadTypeItem>
         );
       })}
