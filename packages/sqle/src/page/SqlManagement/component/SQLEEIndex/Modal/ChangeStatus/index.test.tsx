821f57bb56e494901e16a96d265e0af2213fc505
sunjinkang <2060037942@qq.com>
2024-01-16 10:40:50 +0800

    [test]: add test for sql manage status operation

--- packages/sqle/src/page/SqlManagement/component/SQLEEIndex/Modal/ChangeStatus/index.test.tsx
+++ packages/sqle/src/page/SqlManagement/component/SQLEEIndex/Modal/ChangeStatus/index.test.tsx
@@ -1,33 +1,28 @@
 import { act, cleanup, fireEvent, screen } from '@testing-library/react';
-import AssignmentSingle from '.';
+import ChangeStatus from '.';
 import { superRender } from '../../../../../../testUtils/customRender';
-import user from '../../../../../../testUtils/mockApi/user';
 import { mockUseCurrentProject } from '@actiontech/shared/lib/testUtil/mockHook/mockUseCurrentProject';
 import { ModalName } from '../../../../../../data/ModalName';
 import { sqlManageListData } from '../../../../../../testUtils/mockApi/sqlManage/data';
 import sqlManage from '../../../../../../testUtils/mockApi/sqlManage';
 import { mockProjectInfo } from '@actiontech/shared/lib/testUtil/mockHook/data';
-import {
-  getAllBySelector,
-  getBySelector
-} from '@actiontech/shared/lib/testUtil/customQuery';
-import { userTipListData } from '../../../../../../testUtils/mockApi/user/data';
+import { getAllBySelector } from '@actiontech/shared/lib/testUtil/customQuery';
 import EventEmitter from '../../../../../../utils/EventEmitter';
 import { useDispatch } from 'react-redux';
 import EmitterKey from '../../../../../../data/EmitterKey';
+import { BatchUpdateSqlManageReqStatusEnum } from '@actiontech/shared/lib/api/sqle/service/common.enum';
 
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
   useDispatch: jest.fn()
 }));
 
-describe('page/SqlManagement/AssignmentSingle', () => {
+describe('page/SqlManagement/ChangeStatus', () => {
   const dispatchSpy = jest.fn();
 
   beforeEach(() => {
     (useDispatch as jest.Mock).mockImplementation(() => dispatchSpy);
     mockUseCurrentProject();
-    user.mockAllApi();
     sqlManage.mockAllApi();
     jest.useFakeTimers();
   });
@@ -39,11 +34,11 @@ describe('page/SqlManagement/AssignmentSingle', () => {
   });
 
   const customRender = (data?: boolean) => {
-    return superRender(<AssignmentSingle />, undefined, {
+    return superRender(<ChangeStatus />, undefined, {
       initStore: {
         sqleManagement: {
           modalStatus: {
-            [ModalName.Assignment_Member_Single]: data ?? true
+            [ModalName.Change_Status_Single]: data ?? true
           },
           selectSqleManagement: sqlManageListData.data[0]
         }
@@ -56,37 +51,34 @@ describe('page/SqlManagement/AssignmentSingle', () => {
     expect(baseElement).toMatchSnapshot();
   });
 
-  it('update single assign and submit change', async () => {
+  it('change status and submit data', async () => {
     const eventEmitSpy = jest.spyOn(EventEmitter, 'emit');
-    const optionRequest = user.getUserTip();
     const submitRequest = sqlManage.batchUpdateSqlManage();
     const { baseElement } = customRender();
     await act(async () => jest.advanceTimersByTime(3000));
-    expect(optionRequest).toBeCalledWith({
-      filter_project: mockProjectInfo.projectName
-    });
     expect(baseElement).toMatchSnapshot();
-    expect(screen.getByText('指派负责人')).toBeInTheDocument();
-    fireEvent.mouseDown(getBySelector('#assignees'));
-    await act(async () => jest.advanceTimersByTime(300));
-    const options = getAllBySelector('.ant-select-item-option');
-    fireEvent.click(options[0]);
+    expect(screen.getByText('变更状态')).toBeInTheDocument();
+    expect(screen.getByText('当前SQL状态')).toBeInTheDocument();
+    expect(screen.getByText('忽略')).toBeInTheDocument();
+    const radioOptions = getAllBySelector('.ant-radio-input');
+    expect(radioOptions.length).toBe(3);
+    fireEvent.click(radioOptions?.[1]);
     await act(async () => jest.advanceTimersByTime(300));
     fireEvent.click(screen.getByText('确 认'));
     await act(async () => jest.advanceTimersByTime(300));
     expect(submitRequest).toBeCalledWith({
       project_name: mockProjectInfo.projectName,
       sql_manage_id_list: [sqlManageListData.data[0].id],
-      assignees: [userTipListData[0].user_id]
+      status: BatchUpdateSqlManageReqStatusEnum.ignored
     });
     await act(async () => jest.advanceTimersByTime(3000));
-    expect(screen.getByText('指派负责人成功')).toBeInTheDocument();
+    expect(screen.getByText('更新SQL状态成功')).toBeInTheDocument();
     await act(async () => jest.advanceTimersByTime(3300));
     expect(dispatchSpy).toBeCalledTimes(2);
     expect(dispatchSpy).toBeCalledWith({
       type: 'sqleManagement/updateModalStatus',
       payload: {
-        modalName: ModalName.Assignment_Member_Single,
+        modalName: ModalName.Change_Status_Single,
         status: false
       }
     });
@@ -102,14 +94,14 @@ describe('page/SqlManagement/AssignmentSingle', () => {
     const { baseElement } = customRender();
     await act(async () => jest.advanceTimersByTime(3000));
     expect(baseElement).toMatchSnapshot();
-    expect(screen.getByText('指派负责人')).toBeInTheDocument();
+    expect(screen.getByText('变更状态')).toBeInTheDocument();
     fireEvent.click(screen.getByText('取 消'));
     await act(async () => jest.advanceTimersByTime(300));
     expect(dispatchSpy).toBeCalledTimes(2);
     expect(dispatchSpy).toBeCalledWith({
       type: 'sqleManagement/updateModalStatus',
       payload: {
-        modalName: ModalName.Assignment_Member_Single,
+        modalName: ModalName.Change_Status_Single,
         status: false
       }
     });
