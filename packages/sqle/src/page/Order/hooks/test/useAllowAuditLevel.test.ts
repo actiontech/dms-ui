d2cff4e1b806582bdc22df72bdd83338f3287a8e
xinbinyan <404928130@qq.com>
2024-01-18 16:49:11 +0800

    case useAllowAuditLevel

---
+++ packages/sqle/src/page/Order/hooks/test/useAllowAuditLevel.test.ts
@@ -0,0 +1,55 @@
+import { useAllowAuditLevel } from '../useAllowAuditLevel';
+
+import { act, cleanup, renderHook } from '@testing-library/react';
+import { WorkflowTemplateDetailResV1AllowSubmitWhenLessAuditLevelEnum } from '@actiontech/shared/lib/api/sqle/service/common.enum';
+import order from '../../../../testUtils/mockApi/order';
+
+describe('sqle/order/hooks/useAllowAuditLevel', () => {
+  const customRender = () => {
+    return renderHook(() => useAllowAuditLevel());
+  }
+
+  let requestGetWorkflowTemplate: jest.SpyInstance;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    requestGetWorkflowTemplate = order.getWorkflowTemplate();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+    cleanup();
+  });
+
+  it('render use judgeAuditLevel fn', async () => {
+    const setBtnDisabledFn = jest.fn();
+    const resetBtnDisabledFn = jest.fn();
+    const { result } = customRender();
+
+    expect(result.current.disabledOperatorOrderBtnTips).toBe('');
+    await act(async () => {
+      result.current.setDisabledOperatorOrderBtnTips('text');
+    })
+    expect(result.current.disabledOperatorOrderBtnTips).toBe('text');
+
+    await act(async () => {
+      result.current.judgeAuditLevel(
+        [
+          {
+            projectName: 'a',
+            instanceName: 'instance a',
+            currentAuditLevel:
+              WorkflowTemplateDetailResV1AllowSubmitWhenLessAuditLevelEnum.normal
+          }
+        ],
+        setBtnDisabledFn,
+        resetBtnDisabledFn
+      );
+      await act(async () => jest.advanceTimersByTime(3000));
+    });
+    expect(requestGetWorkflowTemplate).toBeCalled;
+    expect(resetBtnDisabledFn).toBeCalled();
+    expect(result.current.disabledOperatorOrderBtnTips).toBe('');
+  });
+});
