843d292e2819e1d22da84cf71e0c447b1bb5ccc2
xinbinyan <404928130@qq.com>
2024-01-18 17:10:31 +0800

    case useAuditResultRuleInfo

---
+++ packages/sqle/src/page/Order/hooks/test/useAuditResultRuleInfo.test.ts
@@ -0,0 +1,45 @@
+import useAuditResultRuleInfo from '../useAuditResultRuleInfo';
+
+import { act, cleanup, renderHook } from '@testing-library/react';
+import rule_template from '../../../../testUtils/mockApi/rule_template';
+
+const auditResultData = [
+  {
+    db_type: 'mysql',
+    level: '',
+    message: 'mes str',
+    rule_name: 'rule name'
+  }
+];
+
+describe('sqle/order/hooks/useAuditResultRuleInfo', () => {
+
+  let requestGetRuleList: jest.SpyInstance;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    requestGetRuleList = rule_template.getRuleList();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+    cleanup();
+  });
+
+  it('render when auditResult is empty', () => {
+    renderHook(() => useAuditResultRuleInfo([]));
+  });
+
+  it('render when auditResult', async () => {
+    const { result } = renderHook(() => useAuditResultRuleInfo(auditResultData));
+    await act(async () => jest.advanceTimersByTime(3300));
+    expect(requestGetRuleList).toBeCalled();
+    expect(requestGetRuleList).toBeCalledWith({
+      filter_db_type: undefined,
+      filter_rule_names: 'rule name'
+    });
+
+    expect(result.current.auditResultRuleInfo).toEqual(auditResultData);
+  });
+});
