01e5bdd8abebce2704a95d88fb9d556a06c545a0
xinbinyan <404928130@qq.com>
2024-01-19 15:09:42 +0800

    case ChartTooltip

---
+++ packages/sqle/src/components/ChartCom/ChartTooltip/useGetConfig.test.ts
@@ -0,0 +1,17 @@
+import { renderHook } from '@testing-library/react';
+
+import useGetConfig from './useGetConfig';
+import { SupportTheme, ThemeData } from '../../../theme';
+
+const themeData = ThemeData[SupportTheme.LIGHT];
+
+describe('sqle/components/ChartCom/ChartTooltip/useGetConfig', () => {
+  const customRender = () => {
+    return renderHook(() => useGetConfig(themeData.sqleTheme));
+  }
+
+  it('render useGetConfig when width is 200', () => {
+    const { result } = customRender();
+    expect(result.current.getDomStyles(200)).toMatchSnapshot()
+  });
+});
