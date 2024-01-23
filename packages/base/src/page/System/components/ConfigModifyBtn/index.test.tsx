5eb17bc9213d737b6e1bb6fbbbc7576590058a33
xinbinyan <404928130@qq.com>
2024-01-23 14:19:21 +0800

    case ConfigModifyBtn

---
+++ packages/base/src/page/System/components/ConfigModifyBtn/index.test.tsx
@@ -0,0 +1,31 @@
+import { cleanup, fireEvent, act } from '@testing-library/react';
+import { renderWithTheme } from '@actiontech/shared/lib/testUtil/customRender';
+import { getBySelector } from '@actiontech/shared/lib/testUtil/customQuery';
+
+import ConfigModifyBtn from '.';
+
+describe('base/System/components/ConfigModifyBtn', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    cleanup();
+  });
+
+  it('render snap', async () => {
+    const onClickFn = jest.fn();
+    const { baseElement } = renderWithTheme(<ConfigModifyBtn onClick={onClickFn} />);
+    expect(baseElement).toMatchSnapshot();
+
+    const btnEle = getBySelector('.ant-btn', baseElement);
+
+    fireEvent.mouseOver(btnEle);
+    await act(async () => jest.advanceTimersByTime(500));
+    expect(baseElement).toMatchSnapshot();
+
+    fireEvent.click(btnEle);
+    expect(onClickFn).toBeCalled();
+  })
+});
