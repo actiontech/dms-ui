44c6515c36aaa013f81c7dfe855b91bfc68b02c4
xinbinyan <404928130@qq.com>
2024-01-24 14:29:43 +0800

    case Wechat

--- packages/base/src/page/System/PushNotification/Wechat/components/ConfigField.test.tsx
+++ packages/base/src/page/System/PushNotification/Wechat/components/ConfigField.test.tsx
@@ -1,15 +1,51 @@
 import { renderWithTheme } from '@actiontech/shared/lib/testUtil/customRender';
+import { cleanup, fireEvent, act, screen } from '@testing-library/react';
+import { getBySelector } from '@actiontech/shared/lib/testUtil/customQuery';
 
 import { Form } from 'antd';
 import ConfigField from './ConfigField';
 
 describe('base/System/PushNotification/Wechat/ConfigField', () => {
-  it('render snap', () => {
-    const { baseElement } = renderWithTheme(
+  const customRender = () => {
+    return renderWithTheme(
       <Form>
         <ConfigField />
       </Form>
-    );
+    )
+  };
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+    cleanup();
+  });
+
+  it('render snap', () => {
+    const { baseElement } = customRender();
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('render validate agent_id item', async () => {
+    const { baseElement } = customRender();
+
+    fireEvent.change(getBySelector('#agent_id', baseElement), {
+      target: {
+        value: 'agent_id'
+      }
+    });
+    await act(async () => jest.advanceTimersByTime(500));
+    expect(baseElement).toMatchSnapshot();
+
+    fireEvent.change(getBySelector('#agent_id', baseElement), {
+      target: {
+        value: '123'
+      }
+    });
+    await act(async () => jest.advanceTimersByTime(500));
     expect(baseElement).toMatchSnapshot();
   });
 });
