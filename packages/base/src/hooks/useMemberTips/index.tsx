48102d9b84b8833a2499cd3442719e5620d52523
lizhensheng <lizhensheng@actionsky.com>
2024-01-25 20:05:35 +0800

    [feature]: Add member tips

---
+++ packages/base/src/hooks/useMemberTips/index.tsx
@@ -0,0 +1,72 @@
+import { useBoolean } from 'ahooks';
+import React, { useMemo } from 'react';
+import { ResponseCode } from '@actiontech/shared/lib/enum';
+import { Select, Space, Typography } from 'antd';
+import dms from '@actiontech/shared/lib/api/base/service/dms';
+import { AvatarCom } from '@actiontech/shared';
+import { IListMemberTipsParams } from '@actiontech/shared/lib/api/base/service/dms/index.d';
+import { IListMemberTipsItem } from '@actiontech/shared/lib/api/base/service/common';
+
+const useMemberTips = () => {
+  const [memberTips, setMemberTips] = React.useState<IListMemberTipsItem[]>([]);
+  const [loading, { setTrue, setFalse }] = useBoolean();
+
+  const updateMemberTips = React.useCallback(
+    (params: IListMemberTipsParams) => {
+      setTrue();
+      dms
+        .ListMemberTips(params)
+        .then((res) => {
+          if (res.data.code === ResponseCode.SUCCESS) {
+            setMemberTips(res.data?.data ?? []);
+          } else {
+            setMemberTips([]);
+          }
+        })
+        .catch(() => {
+          setMemberTips([]);
+        })
+        .finally(() => {
+          setFalse();
+        });
+    },
+    [setFalse, setTrue]
+  );
+
+  const generateMemberSelectOptions = React.useCallback(() => {
+    return memberTips.map((member) => {
+      return (
+        <Select.Option
+          key={member.user_id}
+          value={member.user_id ?? ''}
+          label={member.user_name}
+        >
+          {member.user_name}
+        </Select.Option>
+      );
+    });
+  }, [memberTips]);
+
+  const memberOptions = useMemo(() => {
+    return memberTips.map((member) => ({
+      text: member.user_name,
+      label: (
+        <Space>
+          <AvatarCom noTips size="small" name={member.user_name} />
+          <Typography.Text>{member.user_name}</Typography.Text>
+        </Space>
+      ),
+      value: member.user_id
+    }));
+  }, [memberTips]);
+
+  return {
+    memberTips,
+    loading,
+    updateMemberTips,
+    generateMemberSelectOptions,
+    memberOptions
+  };
+};
+
+export default useMemberTips;
