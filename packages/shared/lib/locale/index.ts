1d51a89ed9c149c9889264855ffe2b04b34da213
zzyangh <799463087@qq.com>
2024-01-26 15:37:10 +0800

    [feature](shared/EnterpriseFeatureDisplay): edition comparison update

--- packages/shared/lib/locale/index.ts
+++ packages/shared/lib/locale/index.ts
@@ -1,4 +1,10 @@
 import { TemplateKeyPath } from '../types/common.type';
 import commonZhCN from './zh-CN';
+import { TOptions } from 'i18next';
+import i18n from 'i18next';
 
 export type I18nKey = TemplateKeyPath<typeof commonZhCN.translation>;
+
+export const t = (key: I18nKey, opts: TOptions = {}) => {
+  return i18n.t(key, opts);
+};
