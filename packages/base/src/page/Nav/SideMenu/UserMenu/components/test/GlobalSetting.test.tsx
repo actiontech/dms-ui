3d5f9f26736097df5a6d23148083dd8deebc8dd2
xinbinyan <404928130@qq.com>
2024-01-30 15:05:52 +0800

    case GlobalSetting

---
+++ packages/base/src/page/Nav/SideMenu/UserMenu/components/test/GlobalSetting.test.tsx
@@ -0,0 +1,69 @@
+import GlobalSetting from '../GlobalSetting';
+import { useNavigate } from 'react-router-dom';
+import { SupportTheme } from 'sqle/src/theme';
+
+import { superRender } from '../../../../../../testUtils/customRender';
+import { act, cleanup, fireEvent, screen } from '@testing-library/react';
+import { getAllBySelector, getBySelector } from '@actiontech/shared/lib/testUtil/customQuery';
+
+jest.mock('react-router-dom', () => {
+  return {
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+  };
+});
+
+describe('base/page/Nav/SideMenu/GlobalSetting', () => {
+  const navigateSpy = jest.fn();
+  const customRender = (isAdmin=false) => {
+    return superRender(
+      <GlobalSetting
+        updateTheme={jest.fn()}
+        theme={SupportTheme.LIGHT}
+        isAdmin={isAdmin}
+      />
+    );
+  };
+
+  beforeEach(() => {
+    (useNavigate as jest.Mock).mockImplementation(() => navigateSpy);
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    cleanup();
+  });
+
+  it('render snap when is not admin', async () => {
+    const { baseElement } = customRender();
+    expect(baseElement).toMatchSnapshot();
+
+    const iconSystem = getBySelector('.custom-icon-global-system', baseElement);
+    fireEvent.click(iconSystem);
+    await act(async () => jest.advanceTimersByTime(500));
+    expect(baseElement).toMatchSnapshot();
+
+    expect(screen.getByText('查看规则')).toBeInTheDocument();
+    expect(getAllBySelector('.content-item-text').length).toBe(1);
+    fireEvent.click(screen.getByText('查看规则'));
+    await act(async () => jest.advanceTimersByTime(500));
+    expect(navigateSpy).toBeCalledWith('/sqle/rule');
+  });
+
+  it('render snap when is admin', async () => {
+    const { baseElement } = customRender(true);
+    expect(baseElement).toMatchSnapshot();
+
+    const iconSystem = getBySelector('.custom-icon-global-system', baseElement);
+    fireEvent.click(iconSystem);
+    await act(async () => jest.advanceTimersByTime(500));
+    expect(baseElement).toMatchSnapshot();
+
+    expect(screen.getByText('用户中心')).toBeInTheDocument();
+    expect(getAllBySelector('.content-item-text').length).toBe(5);
+    fireEvent.click(screen.getByText('用户中心'));
+    await act(async () => jest.advanceTimersByTime(500));
+    expect(navigateSpy).toBeCalledWith('/userCenter');
+  });
+});
