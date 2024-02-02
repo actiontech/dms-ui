daed519ea79ab076902b456c8c32afd15112065c
zzyangh <799463087@qq.com>
2024-02-02 18:05:19 +0800

    case GlobalRuleTemplate/RuleTemplateDetail

--- packages/sqle/src/page/RuleTemplate/EditRuleTemplate/index.test.tsx
+++ packages/sqle/src/page/RuleTemplate/EditRuleTemplate/index.test.tsx
@@ -2,8 +2,11 @@ import { act, cleanup, fireEvent, screen } from '@testing-library/react';
 import { renderWithThemeAndRedux } from '../../../testUtils/customRender';
 import EditRuleTemplate from '.';
 import { ruleListMockData } from '../../../testUtils/mockApi/rule_template/data';
+import { RuleParamResV1TypeEnum } from '@actiontech/shared/lib/api/sqle/service/common.enum';
 
 describe('sqle/RuleTemplate/EditRuleTemplate', () => {
+  const closeSpy = jest.fn();
+  const submitSpy = jest.fn();
   beforeEach(() => {
     jest.useFakeTimers();
   });
@@ -14,8 +17,6 @@ describe('sqle/RuleTemplate/EditRuleTemplate', () => {
   });
 
   it('should match snap shot', async () => {
-    const closeSpy = jest.fn();
-    const submitSpy = jest.fn();
     const { baseElement } = renderWithThemeAndRedux(
       <EditRuleTemplate
         visible
@@ -33,5 +34,46 @@ describe('sqle/RuleTemplate/EditRuleTemplate', () => {
     fireEvent.click(screen.getByText('取 消'));
     await act(async () => jest.advanceTimersByTime(100));
     expect(closeSpy).toBeCalledTimes(1);
+  });
+
+  it('when dataSource is empty object', async () => {
+    const { baseElement } = renderWithThemeAndRedux(
+      <EditRuleTemplate
+        visible
+        title="编辑规则"
+        dataSource={{}}
+        onClosed={closeSpy}
+        onSubmit={submitSpy}
+      />
+    );
+    await act(async () => jest.advanceTimersByTime(3000));
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('when param type is boolean', async () => {
+    const { baseElement } = renderWithThemeAndRedux(
+      <EditRuleTemplate
+        visible
+        title="编辑规则"
+        dataSource={{
+          ...ruleListMockData[0],
+          params: [
+            {
+              key: 'first_key_test',
+              value: 'true',
+              desc: 'test',
+              type: RuleParamResV1TypeEnum.bool
+            }
+          ]
+        }}
+        onClosed={closeSpy}
+        onSubmit={submitSpy}
+      />
+    );
+    await act(async () => jest.advanceTimersByTime(3000));
+    expect(baseElement).toMatchSnapshot();
+    fireEvent.click(screen.getByText('保 存'));
+    await act(async () => jest.advanceTimersByTime(100));
+    expect(submitSpy).toBeCalledTimes(1);
   });
 });
