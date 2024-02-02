57e60978b637b25a4c358d3a678ba5fdd749da2e
lizhensheng <lizhensheng@actionsky.com>
2024-02-02 11:32:08 +0800

    [test]: fix unit tests error

--- packages/base/src/page/DataExportManagement/Create/components/SubmitWorkflow/__tests__/index.test.tsx
+++ packages/base/src/page/DataExportManagement/Create/components/SubmitWorkflow/__tests__/index.test.tsx
@@ -18,11 +18,9 @@ import {
   createSpyFailResponse,
   createSpySuccessResponse
 } from '@actiontech/shared/lib/testUtil/mockApi';
-import MockDate from 'mockdate';
 
 describe('test base/DataExport/Create/SubmitWorkflow', () => {
   beforeEach(() => {
-    MockDate.set('2024-01-31');
     dataExport.BatchGetDataExportTask();
     dataExport.ListDataExportTaskSQLs();
     jest.useFakeTimers();
@@ -30,7 +28,6 @@ describe('test base/DataExport/Create/SubmitWorkflow', () => {
     mockUseCreateDataExportReduxManage();
   });
   afterEach(() => {
-    MockDate.reset();
     jest.clearAllMocks();
     jest.clearAllTimers();
     jest.useRealTimers();
