986d5aae285077131cd1be5161976d2c7eb7edc9
sunjinkang <2060037942@qq.com>
2024-02-21 17:19:28 +0800

    [test]: add uncover test for report statics

---
+++ packages/sqle/src/page/ReportStatistics/index.test.tsx
@@ -0,0 +1,23 @@
+import { cleanup } from '@testing-library/react';
+import { renderWithTheme } from '../../testUtils/customRender';
+import { mockThemeStyleData } from '../../testUtils/mockHooks/mockThemeStyleData';
+
+import ReportStatistics from '.';
+
+describe('sqle/ReportStatistics', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    mockThemeStyleData();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+    cleanup();
+  });
+
+  it('should match snap shot', async () => {
+    const { baseElement } = renderWithTheme(<ReportStatistics />);
+    expect(baseElement).toMatchSnapshot();
+  });
+});
