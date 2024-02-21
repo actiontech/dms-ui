249affc35be6f87634aa318ad9bc045da94eb073
sunjinkang <2060037942@qq.com>
2024-02-21 17:19:28 +0800

    [test]: add uncover test for report statics

--- packages/sqle/src/page/ReportStatistics/EEIndex/component/charts/DatabaseSourceOrder/index.test.tsx
+++ packages/sqle/src/page/ReportStatistics/EEIndex/component/charts/DatabaseSourceOrder/index.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, act } from '@testing-library/react';
+import { cleanup, act, screen, fireEvent } from '@testing-library/react';
 import { renderWithThemeAndRedux } from '../../../../../../testUtils/customRender';
 
 import statistic from '../../../../../../testUtils/mockApi/statistic';
@@ -41,5 +41,23 @@ describe('ReportStatistics/DatabaseSourceOrder', () => {
     await act(async () => jest.advanceTimersByTime(3000));
     expect(baseElement).toMatchSnapshot();
     expect(requestPlotsData).toBeCalled();
+    expect(screen.getByText('刷新')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('刷新'));
+    await act(async () => jest.advanceTimersByTime(3000));
+    expect(requestPlotsData).toBeCalled();
+  });
+
+  it('render with miss data', async () => {
+    requestPlotsData.mockImplementation(() =>
+      createSpySuccessResponse({
+        data: {
+          instance_total_num: 98,
+          instance_type_percents: [{ percent: 98 }]
+        }
+      })
+    );
+    const { baseElement } = customRender();
+    await act(async () => jest.advanceTimersByTime(300));
+    expect(baseElement).toMatchSnapshot();
   });
 });
