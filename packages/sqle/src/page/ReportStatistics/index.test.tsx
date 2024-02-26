be437fde15de118c43e5b03f4ca98a19a8b99f08
sunjinkang <2060037942@qq.com>
2024-02-26 13:13:43 +0800

    [fix]: fix test error

--- packages/sqle/src/page/ReportStatistics/index.test.tsx
+++ packages/sqle/src/page/ReportStatistics/index.test.tsx
@@ -4,9 +4,20 @@ import { mockThemeStyleData } from '../../testUtils/mockHooks/mockThemeStyleData
 
 import ReportStatistics from '.';
 import { mockUseCurrentUser } from '@actiontech/shared/lib/testUtil/mockHook/mockUseCurrentUser';
+import { ignoreAntdPlotsAttr } from '@actiontech/shared/lib/testUtil/common';
+import statistic from '../../testUtils/mockApi/statistic';
+
+jest.mock('react-router-dom', () => {
+  return {
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn()
+  };
+});
 
 describe('sqle/ReportStatistics', () => {
+  ignoreAntdPlotsAttr();
   beforeEach(() => {
+    statistic.mockAllApi();
     jest.useFakeTimers();
     mockThemeStyleData();
     mockUseCurrentUser();
