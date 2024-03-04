feee99bc06ec5c7608c3823a5fff2fe70b72f6b6
lizhensheng <lizhensheng@actionsky.com>
2024-03-04 11:10:59 +0800

    [test]: update ci test scripts

--- scripts/jest/run-ci-ee.sh
+++ scripts/jest/run-ci-ee.sh
@@ -5,14 +5,26 @@ shard_count=${2:-1}
 
 export JEST_TEST_VERSION_ENV="ee"
 pnpm test:clean
-pnpm jest --ci --logHeapUsage --maxWorkers=50% --silent --watchAll=false --filter='<rootDir>/scripts/jest/custom-filter' --coverage --coverageDirectory=coverage --json --testLocationInResults --outputFile=coverage/report.json --shard=$shard_index/$shard_count
+pnpm jest --ci --maxWorkers=50% --silent --watchAll=false --filter='<rootDir>/scripts/jest/custom-filter' --coverage --coverageDirectory=coverage --json --testLocationInResults --outputFile=coverage/report.json --shard=$shard_index/$shard_count
 if [ $? -ne 0 ]; then
   echo "Jest test failed."
   exit 1
 fi
 
 mv coverage/report.json coverage/report-$shard_index.json
+if [ $? -ne 0 ]; then
+  echo "Jest test failed."
+  exit 1
+fi
+
+
 mv coverage/coverage-final.json coverage/coverage-final-$shard_index.json
+if [ $? -ne 0 ]; then
+  echo "Jest test failed."
+  exit 1
+fi
+
+rm -rf coverage/lcov-report clover.xml locv.info
 if [ $? -ne 0 ]; then
   echo "Jest test failed."
   exit 1
