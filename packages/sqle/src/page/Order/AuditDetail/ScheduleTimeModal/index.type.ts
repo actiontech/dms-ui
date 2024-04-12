76d16fb79ee01c29351cd2c363be7deaaaae1cb2
lizhensheng <lizhensheng@actionsky.com>
2024-04-12 13:18:14 +0800

    [feature]: (Order) Added new notification reminder before schedule execute

--- packages/sqle/src/page/Order/AuditDetail/ScheduleTimeModal/index.type.ts
+++ packages/sqle/src/page/Order/AuditDetail/ScheduleTimeModal/index.type.ts
@@ -1,8 +1,13 @@
 import { IMaintenanceTimeResV1 } from '@actiontech/shared/lib/api/sqle/service/common';
+import { UpdateWorkflowScheduleReqV2NotifyTypeEnum } from '@actiontech/shared/lib/api/sqle/service/common.enum';
 
 export type ScheduleTimeModalProps = {
   open: boolean;
-  submit: (scheduleTime?: string | undefined) => Promise<void>;
+  submit: (
+    scheduleTime?: string,
+    isNotify?: boolean,
+    notifyType?: UpdateWorkflowScheduleReqV2NotifyTypeEnum
+  ) => Promise<void>;
   closeScheduleModal: () => void;
   maintenanceTime?: IMaintenanceTimeResV1[];
 };
