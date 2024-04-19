31bc7a9af3c53ab4848cd2091ee5f27238fa59e6
zzyangh <799463087@qq.com>
2024-04-19 18:34:14 +0800

    [fix]: code lint

--- packages/base/src/hooks/useProjectTips/index.ce.test.tsx
+++ packages/base/src/hooks/useProjectTips/index.ce.test.tsx
@@ -2,11 +2,7 @@
  * @test_version ce
  */
 import project from '../../testUtils/mockApi/project';
-import {
-  cleanup,
-  renderHook,
-  act,
-} from '@testing-library/react';
+import { cleanup, renderHook, act } from '@testing-library/react';
 import useProjectTips from '.';
 import { mockUseCurrentProject } from '@actiontech/shared/lib/testUtil/mockHook/mockUseCurrentProject';
 
@@ -30,12 +26,10 @@ describe('useProjectTips CE', () => {
     act(() => {
       result.current.updateProjectTips();
     });
-   
+
     await act(async () => jest.advanceTimersByTime(3000));
     expect(getProjectTipsSpy).not.toHaveBeenCalled();
-    expect(result.current.isFixedBusiness).toEqual(
-     false
-    );
+    expect(result.current.isFixedBusiness).toEqual(false);
     expect(result.current.projectBusiness).toEqual([]);
     expect(result.current.loading).toBeFalsy();
   });
