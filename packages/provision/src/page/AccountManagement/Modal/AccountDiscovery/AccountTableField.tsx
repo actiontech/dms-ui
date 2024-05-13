55bc2a2f7873550c2a4af19510790584aac7e7e6
zzyangh <799463087@qq.com>
2024-05-13 10:59:46 +0800

    [feature](AccountManagement):  account discovery

---
+++ packages/provision/src/page/AccountManagement/Modal/AccountDiscovery/AccountTableField.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { AccountTableFieldProps } from '../../index.type';
+import { useTranslation } from 'react-i18next';
+import { AccountTableFieldStyleWrapper } from '../../style';
+import { IDBAccountBody } from '@actiontech/shared/lib/api/provision/service/common';
+import { BasicTag } from '@actiontech/shared';
+import { Space } from 'antd';
+
+const AccountTableField: React.FC<AccountTableFieldProps> = ({
+  data,
+  loading,
+  onChange
+}) => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const { t } = useTranslation();
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+    onChange?.(
+      data?.filter((i) =>
+        newSelectedRowKeys.includes(`${i?.hostname}${i?.user}` as React.Key)
+      )
+    );
+  };
+
+  const columns = [
+    {
+      dataIndex: 'user',
+      title: t('account.list.column.account')
+    },
+    {
+      dataIndex: 'permission_info',
+      title: t('account.list.column.permission'),
+      render: (permission: IDBAccountBody['permission_info']) => {
+        return (
+          <Space>
+            {permission?.grants?.map((item) => {
+              return <BasicTag key={item}>{item}</BasicTag>;
+            })}
+          </Space>
+        );
+      }
+    }
+  ];
+
+  return (
+    <AccountTableFieldStyleWrapper
+      dataSource={data}
+      columns={columns}
+      rowSelection={{
+        selectedRowKeys,
+        onChange: onSelectChange,
+        fixed: true,
+        columnWidth: 60
+      }}
+      loading={loading}
+      scroll={{
+        x: 'max-content',
+        y: 400
+      }}
+      rowKey={(record: IDBAccountBody) => {
+        return `${record?.hostname}${record?.user}`;
+      }}
+    />
+  );
+};
+
+export default AccountTableField;
---
+++ packages/provision/src/page/AccountManagement/Modal/AccountDiscovery/index.tsx
@@ -0,0 +1,88 @@
+import { useBoolean } from 'ahooks';
+import { message, Form, Space } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { ResponseCode } from '@actiontech/shared/lib/enum';
+import { useCurrentProject } from '@actiontech/shared/lib/global';
+import EventEmitter from '../../../../utils/EventEmitter';
+import { BasicButton, BasicDrawer } from '@actiontech/shared';
+import useModalStatus from '~/hooks/useModalStatus';
+import { AccountManagementModalStatus } from '~/store/accountManagement';
+import { EventEmitterKey, ModalName } from '~/data/enum';
+import DataBaseInfoField from './DataBaseInfoField';
+import { AccountDiscoveryFormType } from '../../index.type';
+import dbAccountService from '@actiontech/shared/lib/api/provision/service/db_account/';
+
+const AccountDiscoveryModal: React.FC = () => {
+  const { t } = useTranslation();
+
+  const [form] = Form.useForm<AccountDiscoveryFormType>();
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const [submitLoading, { setFalse: submitFinish, setTrue: startSubmit }] =
+    useBoolean();
+
+  const { toggleModal, visible } = useModalStatus(
+    AccountManagementModalStatus,
+    ModalName.AccountDiscoveryModal
+  );
+
+  const { projectID } = useCurrentProject();
+
+  const onClose = () => {
+    form.resetFields();
+    toggleModal(ModalName.AccountDiscoveryModal, false);
+  };
+
+  const onSubmit = async () => {
+    const values = await form.validateFields();
+    startSubmit();
+    dbAccountService
+      .AuthSyncDBAccount({
+        project_uid: projectID,
+        db_service_uid: values.service,
+        accounts: values.account
+      })
+      .then((res) => {
+        if (res.data.code === ResponseCode.SUCCESS) {
+          messageApi.success(t('account.discovery.syncSuccess'));
+          onClose();
+          EventEmitter.emit(
+            EventEmitterKey.Refresh_Account_Management_List_Table
+          );
+        }
+      })
+      .finally(() => {
+        submitFinish();
+      });
+  };
+
+  return (
+    <BasicDrawer
+      open={visible}
+      size="large"
+      placement="right"
+      title={t('account.discovery.title')}
+      onClose={onClose}
+      footer={
+        <Space>
+          <BasicButton onClick={onClose} disabled={submitLoading}>
+            {t('common.close')}
+          </BasicButton>
+          <BasicButton
+            type="primary"
+            onClick={onSubmit}
+            loading={submitLoading}
+          >
+            {t('account.discovery.syncAccount')}
+          </BasicButton>
+        </Space>
+      }
+    >
+      {contextHolder}
+      <DataBaseInfoField form={form} visible={visible} />
+    </BasicDrawer>
+  );
+};
+
+export default AccountDiscoveryModal;
