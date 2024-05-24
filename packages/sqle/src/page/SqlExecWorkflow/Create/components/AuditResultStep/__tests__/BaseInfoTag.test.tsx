4e3e21f420c81609a10ace1159e839740546d441
lizhensheng <lizhensheng@actionsky.com>
2024-05-24 14:26:11 +0800

    [test]: mock date and update snapshot

--- packages/sqle/src/page/SqlExecWorkflow/Create/components/AuditResultStep/__tests__/BaseInfoTag.test.tsx
+++ packages/sqle/src/page/SqlExecWorkflow/Create/components/AuditResultStep/__tests__/BaseInfoTag.test.tsx
@@ -1,12 +1,15 @@
 import { shallow } from 'enzyme';
 import BaseInfoTag from '../UpdateFormDrawer/BaseInfoTag';
-import { MockSharedStepDetail } from '../../../hooks/mockData';
 import toJson from 'enzyme-to-json';
 import { mockUseCurrentProject } from '@actiontech/shared/lib/testUtil/mockHook/mockUseCurrentProject';
 import { mockUseCurrentUser } from '@actiontech/shared/lib/testUtil/mockHook/mockUseCurrentUser';
+import MockDate from 'mockdate';
+import dayjs from 'dayjs';
 
 describe('test UpdateFormDrawer', () => {
   it('should match snapshot', () => {
+    MockDate.set(dayjs('2024-05-23 12:00:00').valueOf());
+
     mockUseCurrentProject();
     mockUseCurrentUser();
     const wrapper = shallow(<BaseInfoTag />);
