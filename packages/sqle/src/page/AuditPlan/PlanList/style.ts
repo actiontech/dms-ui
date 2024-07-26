865157a567a78607dbdeeed862f2083c48ef1496
lizhensheng <lizhensheng@actionsky.com>
2024-07-26 17:50:20 +0800

    [chore]: remove audit plan

--- packages/sqle/src/page/AuditPlan/PlanList/style.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from '@mui/material/styles';
-import { Space } from 'antd';
-
-export const PlanListTaskTypeButtonStyleWrapper = styled(Space)`
-  .ant-space-item {
-    display: flex;
-  }
-`;
--- packages/sqle/src/page/AuditPlan/UpdatePlan/index.type.ts
+++
@@ -1,3 +0,0 @@
-export type UpdateAuditPlanUrlParams = {
-  auditPlanName: string;
-};
