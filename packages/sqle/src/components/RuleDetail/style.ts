51d511a8ecfab7ecec636ec814e417907bcb1872
lizhensheng <lizhensheng@actionsky.com>
2024-07-26 17:50:20 +0800

    [chore]: remove audit plan

--- packages/sqle/src/components/RuleDetail/style.ts
+++ packages/sqle/src/components/RuleDetail/style.ts
@@ -65,3 +65,116 @@ export const RuleDetailCustomSearchInputStyleWrapper = styled(
     width: 280px;
   }
 `;
+
+export const DetailComStyleWrapper = styled('section')`
+  height: 100%;
+
+  .header-wrapper {
+    padding: 24px 40px;
+    border-bottom: 1px solid
+      ${({ theme }) => theme.sharedTheme.uiToken.colorBorderSecondary};
+
+    .header {
+      display: flex;
+      align-items: center;
+      margin-bottom: 12px;
+
+      h3.header-cont-text {
+        margin-bottom: 0;
+        font-size: 24px;
+        font-weight: 600;
+        line-height: 32px;
+        color: ${({ theme }) => theme.sharedTheme.uiToken.colorTextBase};
+      }
+
+      .refresh-icon {
+        width: 28px;
+        height: 28px;
+        line-height: 28px;
+        margin-left: 8px;
+        color: ${({ theme }) => theme.sharedTheme.uiToken.colorTextTertiary};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+
+    .tag-wrapper {
+      display: flex;
+      align-items: center;
+
+      .custom-tag-item {
+        display: flex;
+        align-items: center;
+        height: 28px;
+        padding: 0 8px 0 6px;
+        border-radius: 4px;
+        border: 1px solid
+          ${({ theme }) => theme.sharedTheme.uiToken.colorBorderSecondary};
+        margin-right: 8px;
+        background-color: ${({ theme }) =>
+          theme.sharedTheme.basic.colorWhite} !important;
+
+        * {
+          font-size: 13px;
+          font-weight: 600;
+          line-height: 20px;
+          color: ${({ theme }) => theme.sharedTheme.uiToken.colorTextSecondary};
+        }
+
+        .custom-icon {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          &.custom-tag-icon {
+            width: 20px;
+            height: 20px;
+            line-height: 20px;
+            text-align: center;
+            border-radius: 3px;
+            margin-right: 6px;
+          }
+
+          &.custom-tag-right-icon {
+            margin-right: 0;
+            margin-left: 6px;
+          }
+
+          &.cursor-pointer {
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+
+  .detail-table-wrapper {
+    height: 100%;
+    position: relative;
+
+    .ant-spin-nested-loading {
+      width: 100%;
+    }
+
+    .actiontech-table-namespace,
+    .ant-spin-container,
+    .ant-spin-nested-loading {
+      height: 100%;
+    }
+
+    .actiontech-table-namespace {
+      padding-bottom: 0;
+    }
+
+    .ant-spin-container {
+      position: relative;
+    }
+
+    .ant-pagination {
+      left: 220px !important;
+      right: none !important;
+      min-width: 730px;
+    }
+  }
+`;
