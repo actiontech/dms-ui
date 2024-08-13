b69577634968f3aa546193e0da97500f97d49cd1
zzyangh <799463087@qq.com>
2024-08-13 16:15:17 +0800

    [fix]: Unified data source link testing information display method

---
+++ packages/shared/lib/components/ReminderInformation/__tests__/index.test.tsx
@@ -0,0 +1,18 @@
+import { superRender } from '../../../testUtil/customRender';
+import ReminderInformation from '..';
+
+describe('shared/lib/components/ReminderInformation', () => {
+  it('render success message', async () => {
+    const { baseElement } = superRender(
+      <ReminderInformation show status="success" message="success message" />
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('render error message', async () => {
+    const { baseElement } = superRender(
+      <ReminderInformation show status="error" message="error message" />
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+});
--- packages/sqle/src/page/SqlExecWorkflow/Create/components/FormStep/SqlAuditInfoForm/SqlAuditInfoFormItem/DatabaseSelectionItems/hooks/useTestDatabaseConnect.tsx
+++ packages/sqle/src/page/SqlExecWorkflow/Create/components/FormStep/SqlAuditInfoForm/SqlAuditInfoFormItem/DatabaseSelectionItems/hooks/useTestDatabaseConnect.tsx
@@ -1,6 +1,5 @@
-import { BasicToolTips } from '@actiontech/shared';
+import { ReminderInformation } from '@actiontech/shared';
 import instance from '@actiontech/shared/lib/api/sqle/service/instance';
-import { TestConnectResultStyleWrapper } from '@actiontech/shared/lib/components/TestDatabaseConnectButton/style';
 import { ResponseCode } from '@actiontech/shared/lib/enum';
 import { useCurrentProject } from '@actiontech/shared/lib/global';
 import { useBoolean } from 'ahooks';
@@ -9,7 +8,6 @@ import { useTranslation } from 'react-i18next';
 import { DatabaseSelectionItemProps } from '../../../index.type';
 import { IBatchCheckInstanceIsConnectableByNameParams } from '@actiontech/shared/lib/api/sqle/service/instance/index.d';
 import { DatabaseSelectionFields } from '../index.type';
-import { CloseCircleOutlined, CheckCircleOutlined } from '@actiontech/icons';
 
 const useTestDatabaseConnect = ({
   databaseInfo,
@@ -76,24 +74,20 @@ const useTestDatabaseConnect = ({
       }
 
       return (
-        <TestConnectResultStyleWrapper
-          success={!!result.is_instance_connectable}
-        >
-          {!!result.is_instance_connectable && (
-            <TestConnectResultStyleWrapper success>
-              <CheckCircleOutlined className="custom-icon" />
-              {t('common.testDatabaseConnectButton.testSuccess')}
-            </TestConnectResultStyleWrapper>
-          )}
-          {!result.is_instance_connectable && (
-            <BasicToolTips title={result.connect_error_message}>
-              <TestConnectResultStyleWrapper success={false}>
-                <CloseCircleOutlined className="custom-icon" />
-                {t('common.testDatabaseConnectButton.testFailed')}
-              </TestConnectResultStyleWrapper>
-            </BasicToolTips>
-          )}
-        </TestConnectResultStyleWrapper>
+        <>
+          <ReminderInformation
+            show={!!result.is_instance_connectable}
+            status="success"
+            message={t('common.testDatabaseConnectButton.testSuccess')}
+          />
+          <ReminderInformation
+            show={
+              !result.is_instance_connectable && !!result.connect_error_message
+            }
+            status="error"
+            message={result.connect_error_message ?? ''}
+          />
+        </>
       );
     },
     [instanceTestConnectResults.value, t]
