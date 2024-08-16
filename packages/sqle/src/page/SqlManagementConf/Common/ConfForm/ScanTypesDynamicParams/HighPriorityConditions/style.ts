c935e4eb3e7a9a409a9c3560345b51c927410576
LZS911 <932177767@qq.com>
2024-08-16 15:30:51 +0800

    [chore]: fix ci error

--- packages/sqle/src/page/SqlManagementConf/Common/ConfForm/ScanTypesDynamicParams/HighPriorityConditions/style.ts
+++ packages/sqle/src/page/SqlManagementConf/Common/ConfForm/ScanTypesDynamicParams/HighPriorityConditions/style.ts
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 
 export const HighPriorityConditionDescTagStyleWrapper = styled(BasicTag)`
   height: 36px !important;
+
   & .basic-typography-ellipsis {
     color: ${({ theme }) => theme.sharedTheme.uiToken.colorPrimary} !important;
     max-width: 90px;
