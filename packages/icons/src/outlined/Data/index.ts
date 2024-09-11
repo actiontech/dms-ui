224d33e3eab4378ff494fa12247df00b9a2be7f0
zzyangh <799463087@qq.com>
2024-09-11 15:26:54 +0800

    [chore](icons): Add FileVersionOutlined

--- packages/icons/src/outlined/Data/index.ts
+++ packages/icons/src/outlined/Data/index.ts
@@ -1,5 +1,6 @@
 export { default as DoubleDatabaseOutlined } from './DoubleDatabaseOutlined';
 export { default as ExceptionFileOutlined } from './ExceptionFileOutlined';
+export { default as FileVersionOutlined } from './FileVersionOutlined';
 export { default as HorizontalTripleLineOutlined } from './HorizontalTripleLineOutlined';
 export { default as PanelCardOutlined } from './PanelCardOutlined';
 export { default as RiseSquareOutlined } from './RiseSquareOutlined';
