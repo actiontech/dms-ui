a2a9c1021cc15445f112c10bca51cb76b076857b
LZS911 <932177767@qq.com>
2024-09-25 15:29:23 +0800

    [chore]: delete usePreferredLanguages

--- packages/shared/lib/global/index.ts
+++ packages/shared/lib/global/index.ts
@@ -1,4 +1,3 @@
-export { default as usePreferredLanguages } from './usePreferredLanguages';
 export { default as useCurrentUser } from './useCurrentUser';
 export { default as useUserInfo } from './useUserInfo';
 export { default as useCurrentProject } from './useCurrentProject';
