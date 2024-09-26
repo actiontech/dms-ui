2b67992325ce799e75e12bee377dd8b65cb3bca3
LZS911 <932177767@qq.com>
2024-09-26 13:42:13 +0800

    [fix]: sync main to main-ee and resolve unit tests error

--- packages/diagnosis/src/page/Nav/SideMenu/menus/index.tsx
+++ packages/diagnosis/src/page/Nav/SideMenu/menus/index.tsx
@@ -9,14 +9,14 @@ export const DiagnosisMenuItems: GenerateMenuItemsType = ({ navigate }) => [
   },
   {
     order: 1,
-    label: <>{i18n.t('menu.monitorSourceConfig')}</>,
+    label: <>{i18n.t('diagnosisMenu.monitorSourceConfig')}</>,
     key: `/monitor-source-config`,
     icon: <ThunderBulbFilled width={18} height={18} />,
     onClick: () => navigate(`/monitor-source-config`)
   },
   {
     order: 2,
-    label: <>{i18n.t('menu.userManagement')}</>,
+    label: <>{i18n.t('diagnosisMenu.userManagement')}</>,
     key: `/user-management`,
     icon: <UserShieldFilled width={18} height={18} />,
     onClick: () => navigate(`/user-management`)
