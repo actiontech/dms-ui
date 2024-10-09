f7ee109927e28309715521190ec7932ac16ce748
zzyangh <799463087@qq.com>
2024-10-09 14:16:48 +0800

    [test]: Update unit test case

--- packages/sqle/src/page/SqlExecWorkflow/Create/components/FormStep/BaseInfoForm/__tests__/index.test.tsx
+++ packages/sqle/src/page/SqlExecWorkflow/Create/components/FormStep/BaseInfoForm/__tests__/index.test.tsx
@@ -2,9 +2,30 @@ import { shallow } from 'enzyme';
 import BaseInfoForm from '..';
 import toJson from 'enzyme-to-json';
 import { mockThemeStyleData } from '../../../../../../../testUtils/mockHooks/mockThemeStyleData';
+import * as useCreationMode from '../../../../hooks/useCreationMode';
 
 describe('test BaseInfoForm', () => {
+  beforeEach(() => {
+    jest.spyOn(useCreationMode, 'default').mockImplementation(() => ({
+      isCloneMode: false,
+      isAssociationVersionMode: false,
+      versionId: null,
+      versionName: null
+    }));
+  });
   it('should match snapshot', () => {
+    mockThemeStyleData();
+    const wrapper = shallow(<BaseInfoForm />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should match snapshot when isAssociationVersionMode is truth', () => {
+    jest.spyOn(useCreationMode, 'default').mockImplementation(() => ({
+      isCloneMode: false,
+      isAssociationVersionMode: true,
+      versionId: '1',
+      versionName: 'v1-test'
+    }));
     mockThemeStyleData();
     const wrapper = shallow(<BaseInfoForm />);
     expect(toJson(wrapper)).toMatchSnapshot();
