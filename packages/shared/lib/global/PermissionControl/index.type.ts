298f5af3c460d06c79af4b456aef36f782a05142
LZS911 <932177767@qq.com>
2024-10-14 16:55:15 +0800

    [chore]: (PermissionControl) Update component  [skip ci]

--- packages/shared/lib/global/PermissionControl/index.type.ts
+++ packages/shared/lib/global/PermissionControl/index.type.ts
@@ -1,39 +1,7 @@
-import { PopconfirmProps, SpaceProps, TooltipProps } from 'antd';
-import { IBasicButton } from '../../components/BasicButton';
-import { LinkProps } from 'react-router-dom';
+import { ReactNode } from 'react';
 import { PermissionsConstantType } from '../usePermission';
-import { Key } from 'react';
 
-type PermissionControlBase = Omit<IBasicButton, 'children'> & {
+export type PermissionControlProps = {
   permission: PermissionsConstantType;
-  text: string;
+  children: ReactNode;
 };
-
-type PermissionControlWithNormalButton = {
-  actionType?: never;
-};
-
-type PermissionControlWithLink = {
-  actionType: 'navigate-link';
-  link: LinkProps;
-};
-
-type PermissionControlWithConfirmButton = {
-  actionType: 'confirm';
-  confirm: PopconfirmProps;
-};
-
-type PermissionControlWithTooltipsButton = {
-  actionType: 'tooltip';
-  tooltip: TooltipProps;
-};
-
-export type PermissionControlProps =
-  | (PermissionControlBase & PermissionControlWithLink)
-  | (PermissionControlBase & PermissionControlWithConfirmButton)
-  | (PermissionControlBase & PermissionControlWithTooltipsButton)
-  | (PermissionControlBase & PermissionControlWithNormalButton);
-
-export type PermissionControlGroupProps = {
-  actions: Array<PermissionControlProps & { key: Key }>;
-} & SpaceProps;
