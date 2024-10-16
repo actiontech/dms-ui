33203b500f2e0ab09d0519992da837bdbdbc72ac
zzyangh <799463087@qq.com>
2024-10-16 18:04:22 +0800

    [chore]: Add i18n file

---
+++ packages/sqle/src/locale/zh-CN/globalDashboard.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  pageTitle: 'Dashboard',
+  high: '高',
+  medium: '中',
+  low: '低',
+  pendingWorkOrder: '待解决工单',
+  pendingSql: {
+    title: '待解决SQL',
+    column: {
+      source: '来源',
+      status: '状态',
+      firstAppearTime: '出现时间',
+      instance: '所属数据源',
+      project: '所属项目',
+      projectPriority: '项目优先级'
+    }
+  },
+  initiatedWorkOrder: '发起的工单',
+  filter: {
+    project: '项目',
+    instance: '数据源',
+    projectPriority: '项目优先级'
+  }
+};
