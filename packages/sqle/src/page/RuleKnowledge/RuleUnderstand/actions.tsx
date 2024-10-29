a914ad806726b2b7b64bacc8bda0993d8cfae806
zzyangh <799463087@qq.com>
2024-10-29 15:19:32 +0800

    [refactor](RuleKownledge): Operation permissions

---
+++ packages/sqle/src/page/RuleKnowledge/RuleUnderstand/actions.tsx
@@ -0,0 +1,21 @@
+import { t } from '../../../locale';
+import { PERMISSIONS, PermissionControl } from '@actiontech/shared/lib/global';
+import { ActionButton } from '@actiontech/shared';
+
+export const RuleKnowledgeEditActions = (
+  onEdit: () => void,
+  loading: boolean
+): Record<'edit-rule-knowledge', React.ReactNode> => ({
+  'edit-rule-knowledge': (
+    <PermissionControl
+      permission={PERMISSIONS.ACTIONS.SQLE.RULE_KNOWLEDGE.EDIT}
+    >
+      <ActionButton
+        type="primary"
+        text={t('ruleKnowledge.edit')}
+        onClick={onEdit}
+        disabled={loading}
+      />
+    </PermissionControl>
+  )
+});
