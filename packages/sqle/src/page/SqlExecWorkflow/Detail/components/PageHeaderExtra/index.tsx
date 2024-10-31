d315803720e4d0fa8bb6c8afbf9a8c6f803e8ca6
zzyangh <799463087@qq.com>
2024-10-31 18:13:23 +0800

    [fix]: Supplement the work order code caused by conflicts

--- packages/sqle/src/page/SqlExecWorkflow/Detail/components/PageHeaderExtra/index.tsx
+++ packages/sqle/src/page/SqlExecWorkflow/Detail/components/PageHeaderExtra/index.tsx
@@ -1,5 +1,5 @@
 import { BasicButton, EmptyBox, BasicToolTips } from '@actiontech/shared';
-import { Divider, Popconfirm, Space } from 'antd';
+import { Divider, Space } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { useBoolean } from 'ahooks';
 import { WorkflowPageHeaderExtraStyleWrapper } from './style';
@@ -58,9 +58,31 @@ const WorkflowDetailPageHeaderExtra: React.FC<
       {CloneWorkflowAction(executeInOtherInstanceMeta)}
       {BatchRejectWorkflowAction(rejectWorkflowButtonMeta, openRejectModal)}
       {ApproveWorkflowAction(auditPassWorkflowButtonMeta)}
-      {BatchExecWorkflowAction(batchExecutingWorkflowButtonMeta)}
-      {MarkManuallyExecWorkflowAction(manualExecuteWorkflowButtonMeta)}
+      {BatchExecWorkflowAction(batchExecutingWorkflowButtonMeta, executable)}
+      {MarkManuallyExecWorkflowAction(
+        manualExecuteWorkflowButtonMeta,
+        executable
+      )}
 
+      <EmptyBox
+        if={
+          !manualExecuteWorkflowButtonMeta.hidden &&
+          !batchExecutingWorkflowButtonMeta.hidden &&
+          !executable
+        }
+      >
+        <BasicToolTips
+          title={executable_reason}
+          overlayClassName="whitespace-pre-line"
+        >
+          <BasicButton disabled type="primary">
+            {t('execWorkflow.detail.operator.batchSqlExecute')}
+          </BasicButton>
+          <BasicButton disabled type="primary">
+            {t('execWorkflow.detail.operator.markManually')}
+          </BasicButton>
+        </BasicToolTips>
+      </EmptyBox>
       <Space hidden={terminateWorkflowButtonMeta.hidden} size={0}>
         {TerminateWorkflowAction(terminateWorkflowButtonMeta)}
 
