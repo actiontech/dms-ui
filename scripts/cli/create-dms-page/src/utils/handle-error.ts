ac82265b5d494f5cb32691740cd1f34049b967d9
LZS911 <932177767@qq.com>
2024-12-03 16:56:15 +0800

    [chore]: adjust handle error [skip ci]

--- scripts/cli/create-dms-page/src/utils/handle-error.ts
+++ scripts/cli/create-dms-page/src/utils/handle-error.ts
@@ -3,12 +3,10 @@ import { logger } from './logger';
 export function handleError(error: unknown) {
   if (typeof error === 'string') {
     logger.error(error);
-    process.exit(1);
   }
 
   if (error instanceof Error) {
     logger.error(error.message);
-    process.exit(1);
   }
 
   logger.error('Something went wrong. Please try again.');
