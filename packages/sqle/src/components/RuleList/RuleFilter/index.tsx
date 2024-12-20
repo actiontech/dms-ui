eacd1f3092821cc21c8a6087b926c0b9161ee128
zzyangh <799463087@qq.com>
2024-12-20 17:58:37 +0800

    [feature](Rule): Rule category refactoring

---
+++ packages/sqle/src/components/RuleList/RuleFilter/index.tsx
@@ -0,0 +1,20 @@
+import { Form, FormInstance } from 'antd';
+import { FilterContainerStyleWrapper } from '@actiontech/shared/lib/components/ActiontechTable/components/style';
+import RuleFilterCommonFields from './RuleFilterCommonFields';
+import { RuleFilterFieldsType } from '../index.type';
+
+type RuleFilterProps = {
+  form: FormInstance<RuleFilterFieldsType>;
+};
+
+const RuleFilter: React.FC<RuleFilterProps> = ({ form }) => {
+  return (
+    <FilterContainerStyleWrapper className="full-width-element">
+      <Form form={form}>
+        <RuleFilterCommonFields />
+      </Form>
+    </FilterContainerStyleWrapper>
+  );
+};
+
+export default RuleFilter;
