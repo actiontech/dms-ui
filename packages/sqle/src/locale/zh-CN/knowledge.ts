74696b9325dceab52cd74650d81a814b29b9ecaf
zzyangh <799463087@qq.com>
2025-02-11 17:41:58 +0800

    [chore]: Add i18n key [skip ci]

--- packages/sqle/src/locale/zh-CN/knowledge.ts
+++ packages/sqle/src/locale/zh-CN/knowledge.ts
@@ -1,5 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   pageTitle: '知识库',
-  searchPlaceholder: '搜索关键字搜索你感兴趣的SQL语法'
+  searchPlaceholder: '搜索关键字搜索你感兴趣的SQL语法',
+  searchType: {
+    retrieveKnowledgeGraph: '检索知识图谱',
+    retrieveGlobal: '检索全局'
+  }
 };
