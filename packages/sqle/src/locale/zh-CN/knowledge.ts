4ff8a8afe7bf3c2a4bf082a350592350d3474209
zzyangh <799463087@qq.com>
2025-02-12 17:05:21 +0800

    [feature]: Modify custom search component[skip ci]

--- packages/sqle/src/locale/zh-CN/knowledge.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  pageTitle: '知识库',
-  searchPlaceholder: '搜索关键字搜索你感兴趣的SQL语法',
-  searchType: {
-    retrieveKnowledgeGraph: '检索知识图谱',
-    retrieveGlobal: '检索全局'
-  }
-};
