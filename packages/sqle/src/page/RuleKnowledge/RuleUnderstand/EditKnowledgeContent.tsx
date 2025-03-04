0be171478ce48ca487f548ed52adc595faa55b6b
zzyangh <799463087@qq.com>
2025-03-04 13:53:04 +0800

    [feature](RuleKnowledge): Optimize the details page[skip ci]

--- packages/sqle/src/page/RuleKnowledge/RuleUnderstand/EditKnowledgeContent.tsx
+++ packages/sqle/src/page/RuleKnowledge/RuleUnderstand/EditKnowledgeContent.tsx
@@ -2,6 +2,9 @@ import { EditKnowledgeContentProps } from './index.type';
 import rehypeSanitize from 'rehype-sanitize';
 import { RuleKnowledgeEditorStyleWrapper } from '../style';
 import { useTranslation } from 'react-i18next';
+import { markdownPreviewOptions } from '../Common/MarkdownPreview/markdownPreviewOptions';
+import { commands, ICommand } from '@uiw/react-md-editor';
+import { useMemo } from 'react';
 
 const EditKnowledgeContent: React.FC<EditKnowledgeContentProps> = ({
   value,
@@ -10,11 +13,40 @@ const EditKnowledgeContent: React.FC<EditKnowledgeContentProps> = ({
 }) => {
   const { t } = useTranslation();
 
+  const customCommands: ICommand[] = useMemo(() => {
+    return [
+      {
+        name: 'label',
+        keyCommand: 'label',
+        buttonProps: { 'aria-label': 'Insert help' },
+        icon: <span>Label</span>,
+        execute: (state, api) => {
+          api.replaceSelection('```label []```');
+        }
+      },
+      {
+        name: 'Diff',
+        keyCommand: 'Diff',
+        buttonProps: { 'aria-label': 'Insert help' },
+        icon: <span>Diff</span>,
+        execute: (state, api) => {
+          api.replaceSelection(`
+\`\`\`sql_diff
+    ---before
+    ---after
+\`\`\`
+        `);
+        }
+      }
+    ];
+  }, []);
+
   return (
     <RuleKnowledgeEditorStyleWrapper
       height="500px"
       previewOptions={{
-        rehypePlugins: [[rehypeSanitize]]
+        rehypePlugins: [[rehypeSanitize]],
+        components: markdownPreviewOptions?.components
       }}
       value={value}
       onChange={(v) => {
@@ -24,6 +56,7 @@ const EditKnowledgeContent: React.FC<EditKnowledgeContentProps> = ({
       textareaProps={{
         placeholder: t('ruleKnowledge.editorPlaceholder')
       }}
+      commands={[...commands.getCommands(), ...customCommands]}
     />
   );
 };
--- packages/sqle/src/page/RuleKnowledge/index.tsx
+++ packages/sqle/src/page/RuleKnowledge/index.tsx
@@ -1,22 +1,40 @@
 import { useEffect, useState } from 'react';
-import { Card, Typography, Spin } from 'antd';
+import { message } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { useRequest } from 'ahooks';
-import rule_template from '@actiontech/shared/lib/api/sqle/service/rule_template';
+import { RuleTemplateService } from '@actiontech/shared/lib/api';
 import RuleUnderstand from './RuleUnderstand';
 import { EmptyBox, PageHeader, useTypedParams } from '@actiontech/shared';
 import { ResponseCode } from '@actiontech/shared/lib/enum';
-import { RuleKnowledgeContentStyleWrapper } from './style';
-import { DownTriangleOutlined } from '@actiontech/icons';
+import {
+  RuleKnowledgeContentStyleWrapper,
+  RuleKnowledgeStyleWrapper
+} from './style';
 import { ROUTE_PATHS } from '@actiontech/shared/lib/data/routePaths';
+import { RuleKnowledgeEditActions } from './RuleUnderstand/actions';
+import { useBoolean } from 'ahooks';
+import KnowledgeBaseInfo from './components/KnowledgeBaseInfo';
+import PageHeaderExtraAction from './components/PageHeaderExtraAction';
 
 const RuleKnowledge: React.FC = () => {
   const { t } = useTranslation();
+
+  const [isModifying, { setTrue: startModify, setFalse: modifyFinish }] =
+    useBoolean();
+
+  const [hasDirtyData, setHasDirtyData] = useState(false);
+
+  const [editValue, setEditValue] = useState<string>();
+
+  const [submitLoading, { setFalse: submitFinish, setTrue: startSubmit }] =
+    useBoolean();
 
   const { ruleName = '', dbType = '' } =
     useTypedParams<typeof ROUTE_PATHS.SQLE.RULE_KNOWLEDGE.index>();
 
   const [isCustomRule, setIsCustomRule] = useState(false);
+
+  const [messageApi, contextHolder] = message.useMessage();
 
   const {
     data: ruleKnowledgeInfo,
@@ -26,75 +44,124 @@ const RuleKnowledge: React.FC = () => {
   } = useRequest(
     (isCustomType: boolean, type: string) => {
       if (isCustomType) {
-        return rule_template
-          .getCustomRuleKnowledgeV1({ rule_name: ruleName, db_type: type })
-          .then((res) => {
-            return res.data.data;
-          });
-      }
-      return rule_template
-        .getRuleKnowledgeV1({ rule_name: ruleName, db_type: type })
-        .then((res) => {
+        return RuleTemplateService.getCustomRuleKnowledgeV1({
+          rule_name: ruleName,
+          db_type: type
+        }).then((res) => {
           return res.data.data;
         });
+      }
+      return RuleTemplateService.getRuleKnowledgeV1({
+        rule_name: ruleName,
+        db_type: type
+      }).then((res) => {
+        return res.data.data;
+      });
     },
     {
       manual: true
     }
   );
 
+  const submit = () => {
+    startSubmit();
+    const request = isCustomRule
+      ? RuleTemplateService.updateCustomRuleKnowledge({
+          rule_name: ruleName,
+          knowledge_content: editValue,
+          db_type: dbType
+        })
+      : RuleTemplateService.updateRuleKnowledge({
+          rule_name: ruleName,
+          knowledge_content: editValue,
+          db_type: dbType
+        });
+
+    request
+      .then((res) => {
+        if (res.data.code === ResponseCode.SUCCESS) {
+          messageApi.success(t('ruleKnowledge.successTips'));
+          setHasDirtyData(false);
+          modifyFinish();
+          refresh();
+          setEditValue('');
+        }
+      })
+      .finally(() => {
+        submitFinish();
+      });
+  };
+
+  useEffect(() => {
+    if (isModifying) {
+      setEditValue(ruleKnowledgeInfo?.knowledge_content);
+    }
+  }, [ruleKnowledgeInfo?.knowledge_content, isModifying]);
+
   useEffect(() => {
     if (!!ruleName && !!dbType) {
-      rule_template
-        .getRuleListV1({
-          filter_rule_names: ruleName,
-          filter_db_type: dbType
-        })
-        .then((res) => {
-          if (res.data.code === ResponseCode.SUCCESS && res.data.data?.length) {
-            const ruleInfo = res.data.data.find(
-              (i) => i.rule_name === ruleName && i.db_type === dbType
-            );
+      RuleTemplateService.getRuleListV1({
+        filter_rule_names: ruleName,
+        filter_db_type: dbType
+      }).then((res) => {
+        if (res.data.code === ResponseCode.SUCCESS && res.data.data?.length) {
+          const ruleInfo = res.data.data.find(
+            (i) => i.rule_name === ruleName && i.db_type === dbType
+          );
 
-            if (!!ruleInfo?.db_type) {
-              setIsCustomRule(!!ruleInfo.is_custom_rule);
-              getRuleKnowledge(!!ruleInfo.is_custom_rule, ruleInfo.db_type);
-            }
+          if (!!ruleInfo?.db_type) {
+            setIsCustomRule(!!ruleInfo.is_custom_rule);
+            getRuleKnowledge(!!ruleInfo.is_custom_rule, ruleInfo.db_type);
           }
-        });
+        }
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const actions = RuleKnowledgeEditActions(startModify, loading);
+
   return (
-    <>
-      <PageHeader title={t('ruleKnowledge.pageTitle')} />
+    <RuleKnowledgeStyleWrapper>
+      {contextHolder}
+      <PageHeader
+        title={t('ruleKnowledge.pageTitle')}
+        extra={
+          !isModifying ? (
+            actions['edit-rule-knowledge']
+          ) : (
+            <PageHeaderExtraAction
+              hasDirtyData={hasDirtyData}
+              submitLoading={submitLoading}
+              modifyFinish={modifyFinish}
+              onConfirm={() => {
+                modifyFinish();
+                setHasDirtyData(false);
+                setEditValue(ruleKnowledgeInfo?.knowledge_content);
+              }}
+              onSubmit={submit}
+            />
+          )
+        }
+      />
       <RuleKnowledgeContentStyleWrapper>
-        <Card>
-          <Spin spinning={loading}>
-            <Typography.Title level={4}>
-              {ruleKnowledgeInfo?.rule?.desc ?? '-'}
-            </Typography.Title>
-            <Typography.Text type="secondary">
-              {ruleKnowledgeInfo?.rule?.annotation ?? '-'}
-            </Typography.Text>
-          </Spin>
-        </Card>
-        <div className="next-icon-wrap">
-          <DownTriangleOutlined width={18} height={18} />
-        </div>
+        <KnowledgeBaseInfo
+          loading={loading}
+          ruleKnowledgeInfo={ruleKnowledgeInfo}
+        />
         <EmptyBox if={!!dbType}>
           <RuleUnderstand
             loading={loading}
-            ruleName={ruleName}
             content={ruleKnowledgeInfo?.knowledge_content}
-            refresh={refresh}
-            dbType={dbType!}
-            isCustomRule={isCustomRule}
+            isModifying={isModifying}
+            editValue={editValue}
+            setEditValue={setEditValue}
+            setHasDirtyData={setHasDirtyData}
+            ruleName={ruleName}
           />
         </EmptyBox>
       </RuleKnowledgeContentStyleWrapper>
-    </>
+    </RuleKnowledgeStyleWrapper>
   );
 };
 export default RuleKnowledge;
