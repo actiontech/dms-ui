56bcb6e0e02d8c5411847509845c49a44b43d337
LZS911 <932177767@qq.com>
2025-03-05 14:09:07 +0800

    [chore]: adapt to API generator tool upgrade

--- scripts/api/gen_api.sh
+++ scripts/api/gen_api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npm_config_registry=http://10.186.18.19:4873 pnpx @actiontech/cli api-gen -y "$@" && pnpm --filter @actiontech/shared api:exports:g
+npm_config_registry=http://10.186.18.19:4873 pnpx @actiontech/cli api-client -y "$@" && pnpm --filter @actiontech/shared api:exports:g
