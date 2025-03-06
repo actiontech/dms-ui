48b0b8397a5ed289467d03f843369e4e12dd07b1
zzyangh <799463087@qq.com>
2025-03-06 16:04:15 +0800

    [feature](SqlAudit): Support git branch field

--- packages/sqle/src/page/SqlAudit/Create/SQLStatementForm/index.tsx
+++ packages/sqle/src/page/SqlAudit/Create/SQLStatementForm/index.tsx
@@ -1,6 +1,9 @@
 import { useTranslation } from 'react-i18next';
 import { useContext, useEffect } from 'react';
-import { FormItemLabel } from '@actiontech/shared/lib/components/CustomForm';
+import {
+  FormItemLabel,
+  FormItemNoLabel
+} from '@actiontech/shared/lib/components/CustomForm';
 import {
   SQLStatementFormProps,
   UploadTypeEnum
@@ -9,7 +12,10 @@ import {
   BasicInput,
   BasicToolTip,
   EmptyBox,
-  ModeSwitcher
+  ModeSwitcher,
+  BasicButton,
+  BasicSelect,
+  ReminderInformation
 } from '@actiontech/shared';
 import SqlUploadFileCont from './SqlUploadFileCont';
 import { formItemLayout } from '@actiontech/shared/lib/components/CustomForm/style';
@@ -17,12 +23,24 @@ import { FormSubmitStatusContext } from '..';
 import { Form } from 'antd';
 import { uploadTypeOptions } from './index.data';
 import { RingPieFilled } from '@actiontech/icons';
+import useRespConnection from './hooks/useRespConnection';
+import { filterOptionByLabel } from '@actiontech/shared/lib/components/BasicSelect/utils';
 
 const SQLStatementFormWrapper = ({ form }: SQLStatementFormProps) => {
   const { t } = useTranslation();
   const submitLoading = useContext(FormSubmitStatusContext);
 
   const uploadType = Form.useWatch('uploadType', form);
+
+  const {
+    branchOptions,
+    isConnectable,
+    verifyConnection,
+    verifyConnectionLoading,
+    connectionInfoHide,
+    connectionErrorMsg,
+    initConnectionState
+  } = useRespConnection(form);
 
   useEffect(() => {
     form.resetFields([
@@ -32,8 +50,10 @@ const SQLStatementFormWrapper = ({ form }: SQLStatementFormProps) => {
       'zipFile',
       'gitHttpUrl',
       'gitUserName',
-      'gitUserPassword'
+      'gitUserPassword',
+      'gitBranch'
     ]);
+    initConnectionState();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [uploadType]);
 
@@ -66,11 +86,15 @@ const SQLStatementFormWrapper = ({ form }: SQLStatementFormProps) => {
       >
         {/* git form info */}
         <FormItemLabel
+          className="has-required-style"
           name="gitHttpUrl"
           required={true}
           rules={[
             {
-              required: true
+              required: true,
+              message: t('common.form.placeholder.input', {
+                name: t('sqlAudit.create.sqlInfo.uploadLabelEnum.gitUrl')
+              })
             }
           ]}
           {...formItemLayout.fullLine}
@@ -91,9 +115,18 @@ const SQLStatementFormWrapper = ({ form }: SQLStatementFormProps) => {
           />
         </FormItemLabel>
         <FormItemLabel
+          className="has-required-style"
           name="gitUserName"
           label={t('common.username')}
           {...formItemLayout.spaceBetween}
+          rules={[
+            {
+              required: true,
+              message: t('common.form.placeholder.input', {
+                name: t('common.username')
+              })
+            }
+          ]}
         >
           <BasicInput
             disabled={submitLoading}
@@ -103,15 +136,78 @@ const SQLStatementFormWrapper = ({ form }: SQLStatementFormProps) => {
           />
         </FormItemLabel>
         <FormItemLabel
+          className="has-required-style"
           name="gitUserPassword"
           label={t('common.password')}
           {...formItemLayout.spaceBetween}
+          rules={[
+            {
+              required: true,
+              message: t('common.form.placeholder.input', {
+                name: t('common.password')
+              })
+            }
+          ]}
         >
           <BasicInput.Password
             disabled={submitLoading}
             placeholder={t('common.form.placeholder.input', {
               name: t('common.password')
             })}
+          />
+        </FormItemLabel>
+        <FormItemNoLabel {...formItemLayout.spaceBetween}>
+          <BasicButton
+            onClick={verifyConnection}
+            loading={verifyConnectionLoading}
+          >
+            {t('sqlAudit.create.sqlInfo.uploadLabelEnum.verifyConnection')}
+          </BasicButton>
+          <EmptyBox if={!connectionInfoHide}>
+            {!verifyConnectionLoading && isConnectable && (
+              <ReminderInformation
+                status="success"
+                message={t(
+                  'sqlAudit.create.sqlInfo.uploadLabelEnum.connectSuccess'
+                )}
+              />
+            )}
+            {!verifyConnectionLoading && !isConnectable && (
+              <ReminderInformation
+                status="error"
+                message={connectionErrorMsg ?? t('common.unknownError')}
+              />
+            )}
+          </EmptyBox>
+        </FormItemNoLabel>
+        <FormItemLabel
+          className="has-required-style"
+          name="gitBranch"
+          label={t('sqlAudit.create.sqlInfo.uploadLabelEnum.gitBranch')}
+          {...formItemLayout.spaceBetween}
+          rules={[
+            {
+              required: true,
+              message: t('common.form.placeholder.select', {
+                name: t('sqlAudit.create.sqlInfo.uploadLabelEnum.gitBranch')
+              })
+            }
+          ]}
+        >
+          <BasicSelect
+            disabled={submitLoading || !isConnectable}
+            placeholder={
+              !isConnectable
+                ? t(
+                    'sqlAudit.create.sqlInfo.uploadLabelEnum.pleaseVerifyConnection'
+                  )
+                : t('common.form.placeholder.select', {
+                    name: t('sqlAudit.create.sqlInfo.uploadLabelEnum.gitBranch')
+                  })
+            }
+            options={branchOptions}
+            showSearch
+            filterOption={filterOptionByLabel}
           />
         </FormItemLabel>
       </EmptyBox>
