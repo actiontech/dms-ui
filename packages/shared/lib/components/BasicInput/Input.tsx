ac6c99c98dfad4f17d3eecbe4404a5a3a591b7bf
LZS911 <932177767@qq.com>
2025-04-07 14:17:56 +0800

    [fix]: remove redundant ConfigProvider instances

--- packages/shared/lib/components/BasicInput/Input.tsx
+++ packages/shared/lib/components/BasicInput/Input.tsx
@@ -1,10 +1,9 @@
-import { InputProps, ConfigProvider } from 'antd';
+import { InputProps } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { InputRef } from 'antd';
 import { forwardRef } from 'react';
 import classnames from 'classnames';
 import { StyleComponent } from './style';
-import { ComponentControlHeight } from '../../data/common';
 import { CloseOutlined } from '@actiontech/icons';
 
 const Wrapper = StyleComponent('Input');
@@ -17,33 +16,21 @@ const InternalInput: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   const { className, allowClear, ...otherParams } = props;
 
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Input: {
-            controlHeight: ComponentControlHeight.default,
-            controlHeightLG: ComponentControlHeight.lg,
-            controlHeightSM: ComponentControlHeight.sm
-          }
-        }
-      }}
-    >
-      <Wrapper
-        className={classnames('basic-input-wrapper', className)}
-        ref={ref}
-        placeholder={t('common.form.placeholder.input')}
-        {...otherParams}
-        allowClear={
-          allowClear
-            ? {
-                clearIcon: (
-                  <CloseOutlined width={14} height={14} fill="currentColor" />
-                )
-              }
-            : false
-        }
-      />
-    </ConfigProvider>
+    <Wrapper
+      className={classnames('basic-input-wrapper', className)}
+      ref={ref}
+      placeholder={t('common.form.placeholder.input')}
+      {...otherParams}
+      allowClear={
+        allowClear
+          ? {
+              clearIcon: (
+                <CloseOutlined width={14} height={14} fill="currentColor" />
+              )
+            }
+          : false
+      }
+    />
   );
 };
 
