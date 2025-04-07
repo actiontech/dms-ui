335d8cc9495335ef21c049cccf7ae11c8d9a9261
LZS911 <932177767@qq.com>
2025-04-07 14:17:56 +0800

    [fix]: remove redundant ConfigProvider instances

--- packages/shared/lib/components/BasicButton/BasicButton.tsx
+++ packages/shared/lib/components/BasicButton/BasicButton.tsx
@@ -1,5 +1,4 @@
 import classnames from 'classnames';
-import { ConfigProvider } from 'antd';
 import { BasicButtonStyleWrapper } from './style';
 import { BasicButtonProps } from './BasicButton.types';
 
@@ -7,30 +6,18 @@ const BasicButton: React.FC<BasicButtonProps> = (props) => {
   const { children, className, noBorderIcon, ...otherParams } = props;
 
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Button: {
-            controlHeight: 32,
-            controlHeightLG: 36,
-            controlHeightSM: 28
-          }
-        }
-      }}
+    <BasicButtonStyleWrapper
+      className={classnames(
+        {
+          'btn-icon-no-border': noBorderIcon
+        },
+        className,
+        'basic-button-wrapper'
+      )}
+      {...otherParams}
     >
-      <BasicButtonStyleWrapper
-        className={classnames(
-          {
-            'btn-icon-no-border': noBorderIcon
-          },
-          className,
-          'basic-button-wrapper'
-        )}
-        {...otherParams}
-      >
-        {children}
-      </BasicButtonStyleWrapper>
-    </ConfigProvider>
+      {children}
+    </BasicButtonStyleWrapper>
   );
 };
 
