0ae6fe52946b0a2438a89844e07400ae3d9ee62e
zzyangh <799463087@qq.com>
2025-05-08 10:23:02 +0800

    [chore]: Update api[skip ci]

--- packages/shared/lib/api/provision/index.ts
+++ packages/shared/lib/api/provision/index.ts
@@ -1,4 +1,5 @@
 export { default as AuthService } from './service/auth';
+export { default as CustomDbPasswordRuleService } from './service/custom_db_password_rule';
 export { default as DbAccountService } from './service/db_account';
 export { default as DbRoleService } from './service/db_role';
 export { default as PasswordSecuryPolicyService } from './service/password_secury_policy';
