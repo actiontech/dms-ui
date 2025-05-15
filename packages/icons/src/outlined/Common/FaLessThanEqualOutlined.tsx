3e5c6ba9287258067e21489de71db6cf413a2c43
LZS911 <932177767@qq.com>
2025-05-15 14:07:43 +0800

    [feat](icons): build FaLessThanEqualOutlined icon

---
+++ packages/icons/src/outlined/Common/FaLessThanEqualOutlined.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from 'react';
+const FaLessThanEqualOutlined = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      className="FaLessThanEqualOutlined_svg__icon"
+      viewBox="0 0 1024 1024"
+      {...props}
+    >
+      <path d="M896 981.333A42.667 42.667 0 0 1 853.333 1024H170.667a42.667 42.667 0 1 1 0-85.333h682.666A42.667 42.667 0 0 1 896 981.333M870.187 728.79l-622.08-267.178c-25.643-11.008-34.731-36.31-34.731-56.278s9.13-45.312 34.73-56.277l622.08-267.179A42.667 42.667 0 0 0 836.524 3.5L214.357 270.635C161.067 293.547 128 345.173 128 405.333s33.11 111.787 86.4 134.699l622.123 267.179a42.667 42.667 0 0 0 33.664-78.379z" />
+    </svg>
+  );
+};
+export default FaLessThanEqualOutlined;
