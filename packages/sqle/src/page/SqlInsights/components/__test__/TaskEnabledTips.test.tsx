b08f0080effde2d86ca94b7527dfc05743fff10d
zzyangh <799463087@qq.com>
2025-08-01 10:22:02 +0800

    [test]: Update unit testing

--- packages/sqle/src/page/SqlInsights/components/__test__/TaskEnabledTips.test.tsx
+++ packages/sqle/src/page/SqlInsights/components/__test__/TaskEnabledTips.test.tsx
@@ -1,8 +1,20 @@
 import { cleanup, screen, fireEvent } from '@testing-library/react';
 import { superRender } from '@actiontech/shared/lib/testUtil/superRender';
+import { mockUsePermission } from '@actiontech/shared/lib/testUtil';
 import TaskEnabledTips from '../TaskEnabledTips';
 
 describe('TaskEnabledTips', () => {
+  beforeEach(() => {
+    mockUsePermission(
+      {
+        checkActionPermission: jest.fn().mockReturnValue(true)
+      },
+      {
+        useSpyOnMockHooks: true
+      }
+    );
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
     cleanup();
@@ -14,8 +26,8 @@ describe('TaskEnabledTips', () => {
       <TaskEnabledTips onGoToEnable={onGoToEnableMock} />
     );
 
-    expect(screen.getByText('当前管控类型未开启，')).toBeInTheDocument();
-    expect(screen.getByText('去开启')).toBeInTheDocument();
+    expect(screen.getByText('当前管控类型未开启')).toBeInTheDocument();
+    expect(screen.getByText('[去开启]')).toBeInTheDocument();
     expect(baseElement.querySelector('.task-enabled-tips')).toBeInTheDocument();
     expect(baseElement).toMatchSnapshot();
   });
@@ -24,9 +36,28 @@ describe('TaskEnabledTips', () => {
     const onGoToEnableMock = jest.fn();
     superRender(<TaskEnabledTips onGoToEnable={onGoToEnableMock} />);
 
-    const enableLink = screen.getByText('去开启');
+    const enableLink = screen.getByText('[去开启]');
     fireEvent.click(enableLink);
 
     expect(onGoToEnableMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not render component when checkActionPermission is false', () => {
+    mockUsePermission(
+      {
+        checkActionPermission: jest.fn().mockReturnValue(false)
+      },
+      {
+        useSpyOnMockHooks: true
+      }
+    );
+
+    const { baseElement } = superRender(
+      <TaskEnabledTips onGoToEnable={jest.fn()} />
+    );
+
+    expect(baseElement).toMatchSnapshot();
+    expect(screen.getByText('当前管控类型未开启')).toBeInTheDocument();
+    expect(screen.queryByText('[去开启]')).not.toBeInTheDocument();
   });
 });
