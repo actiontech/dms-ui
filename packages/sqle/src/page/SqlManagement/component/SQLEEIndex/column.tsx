66a84b027a2d67f3b348db62777e025b3cf1bdc5
zzyangh <799463087@qq.com>
2025-09-12 16:34:27 +0800

    [chore]: Standardize query parameter naming convention

--- packages/sqle/src/page/SqlManagement/component/SQLEEIndex/column.tsx
+++ packages/sqle/src/page/SqlManagement/component/SQLEEIndex/column.tsx
@@ -190,7 +190,7 @@ const SqlManagementColumn: (
                 target="_blank"
                 to={ROUTE_PATHS.SQLE.SQL_AUDIT.index}
                 params={{ projectID }}
-                queries={{ SQLAuditRecordID: source.sql_source_ids.join(',') }}
+                queries={{ sql_audit_record_id: source.sql_source_ids.join(',') }}
               >
                 {source.sql_source_desc}
               </TypedLink>
