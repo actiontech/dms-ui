bcdd4735b832485e07fe757ec15acd6ddadf1e4c
zzyangh <799463087@qq.com>
2025-09-18 10:25:59 +0800

    [test]: Update unit tests

--- packages/base/src/page/Nav/UserGuideModal/__tests__/index.test.tsx
+++ packages/base/src/page/Nav/UserGuideModal/__tests__/index.test.tsx
@@ -1,38 +1,31 @@
-import { fireEvent, screen, act } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { superRender } from '@actiontech/shared/lib/testUtil/superRender';
 import {
   mockUseCurrentUser,
-  baseMockApi,
-  createSpySuccessResponse
+  baseMockApi
 } from '@actiontech/shared/lib/testUtil';
 import { useDispatch } from 'react-redux';
-import { GetUserSystemEnum } from '@actiontech/shared/lib/api/base/service/common.enum';
-import { updateSystemPreference } from '../../../../store/user';
-import UserGuideModal from '../index';
+import UserGuide from '../index';
+import { mockUseRecentlySelectedZone } from '../../../../testUtils/mockHooks/mockUseRecentlySelectedZone';
+import {
+  SQL_WORKBENCH_FROM_PARAM_NAME,
+  ODC_WORKBENCH_NAME
+} from '@actiontech/dms-kit';
 
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
   useDispatch: jest.fn()
 }));
 
-const originLocation = window.location;
-Object.defineProperty(window, 'location', {
-  value: {
-    ...originLocation
-  },
-  writable: true
-});
-
-describe('UserGuideModal', () => {
+describe('UserGuide', () => {
   const mockDispatch = jest.fn();
-  let getSQLQueryConfigurationSpy: jest.SpyInstance;
-  let updateCurrentUserSpy: jest.SpyInstance;
 
   beforeEach(() => {
-    mockUseCurrentUser();
-    getSQLQueryConfigurationSpy = baseMockApi.cloudBeaver.getSqlQueryUrl();
-    updateCurrentUserSpy = baseMockApi.userCenter.updateCurrentUser();
+    mockUseCurrentUser({ systemPreference: undefined });
+    baseMockApi.cloudBeaver.getSqlQueryUrl();
+    baseMockApi.userCenter.updateCurrentUser();
     (useDispatch as jest.Mock).mockImplementation(() => mockDispatch);
+    mockUseRecentlySelectedZone();
     jest.useFakeTimers();
   });
 
@@ -41,116 +34,20 @@ describe('UserGuideModal', () => {
     jest.clearAllMocks();
   });
 
-  it('should render correctly when systemPreference is undefined', async () => {
-    mockUseCurrentUser({ systemPreference: undefined });
-    const { baseElement } = superRender(<UserGuideModal />);
+  it('should render modal when location not form ODC', async () => {
+    const { baseElement } = superRender(<UserGuide />);
     expect(baseElement).toMatchSnapshot();
     expect(screen.getByText('请选择默认进入的页面')).toBeInTheDocument();
   });
 
-  it('should not render modal when systemPreference is MANAGEMENT', () => {
-    mockUseCurrentUser({
-      systemPreference: GetUserSystemEnum.MANAGEMENT
-    });
-    superRender(<UserGuideModal />);
-
-    expect(screen.queryByText('请选择默认进入的页面')).not.toBeInTheDocument();
-  });
-
-  it('should open CloudBeaver automatically when systemPreference is WORKBENCH', async () => {
-    mockUseCurrentUser({
-      systemPreference: GetUserSystemEnum.WORKBENCH
-    });
-
-    getSQLQueryConfigurationSpy.mockImplementation(() =>
-      createSpySuccessResponse({
-        data: {
-          enable_sql_query: true,
-          sql_query_root_uri: '/cloudbeaver'
-        }
-      })
-    );
-
-    superRender(<UserGuideModal />);
-
-    await act(async () => jest.advanceTimersByTime(3000));
-
-    expect(getSQLQueryConfigurationSpy).toHaveBeenCalled();
-  });
-
-  it('should not open CloudBeaver automatically when systemPreference is WORKBENCH and sql_query_root_uri is the same as location.pathname', async () => {
+  it('should render null when location form ODC', async () => {
     Object.defineProperty(window, 'location', {
       value: {
-        ...originLocation,
-        pathname: '/cloudbeaver'
+        search: `?${SQL_WORKBENCH_FROM_PARAM_NAME}=${ODC_WORKBENCH_NAME}`
       },
       writable: true
     });
-
-    mockUseCurrentUser({
-      systemPreference: GetUserSystemEnum.WORKBENCH
-    });
-
-    getSQLQueryConfigurationSpy.mockImplementation(() =>
-      createSpySuccessResponse({
-        data: {
-          enable_sql_query: true,
-          sql_query_root_uri: '/cloudbeaver'
-        }
-      })
-    );
-
-    superRender(<UserGuideModal />);
-
-    await act(async () => jest.advanceTimersByTime(3000));
-
-    expect(getSQLQueryConfigurationSpy).toHaveBeenCalled();
-  });
-
-  it('should call updateCurrentUser API when confirm button is clicked with MANAGEMENT system', async () => {
-    mockUseCurrentUser({ systemPreference: undefined });
-
-    superRender(<UserGuideModal />);
-
-    const confirmButton = screen.getByText('确认并进入');
-    fireEvent.click(confirmButton);
-
-    await act(async () => jest.advanceTimersByTime(3000));
-
-    expect(updateCurrentUserSpy).toHaveBeenCalledWith({
-      current_user: {
-        system: GetUserSystemEnum.MANAGEMENT
-      }
-    });
-
-    expect(mockDispatch).toHaveBeenCalledWith(
-      updateSystemPreference({ systemPreference: GetUserSystemEnum.MANAGEMENT })
-    );
-  });
-
-  it('should call updateCurrentUser API and CloudBeaver API when confirm button is clicked with WORKBENCH system', async () => {
-    mockUseCurrentUser({ systemPreference: undefined });
-
-    superRender(<UserGuideModal />);
-
-    const workbenchRadio = screen.getByDisplayValue(
-      GetUserSystemEnum.WORKBENCH
-    );
-    fireEvent.click(workbenchRadio);
-
-    const confirmButton = screen.getByText('确认并进入');
-    fireEvent.click(confirmButton);
-
-    await act(async () => jest.advanceTimersByTime(3000));
-
-    expect(updateCurrentUserSpy).toHaveBeenCalledWith({
-      current_user: {
-        system: GetUserSystemEnum.WORKBENCH
-      }
-    });
-
-    expect(mockDispatch).toHaveBeenCalledWith(
-      updateSystemPreference({ systemPreference: GetUserSystemEnum.WORKBENCH })
-    );
+    superRender(<UserGuide />);
+    expect(screen.queryByText('请选择默认进入的页面')).not.toBeInTheDocument();
   });
 });
