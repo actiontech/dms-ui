cdf4a22943359fe2ea66a725b0611354a4d5bfa6
zzyangh <799463087@qq.com>
2025-10-24 18:14:38 +0800

    [fix](DataSource): Modify styles for audit template fields

--- packages/base/src/page/DataSource/components/Form/style.ts
+++ packages/base/src/page/DataSource/components/Form/style.ts
@@ -14,3 +14,47 @@ export const SqlAuditFieldsSubTitleWrapper = styled(Space)`
     line-height: 30px !important;
   }
 `;
+
+export const DataSourceSqlAuditConfigurationStyleWrapper = styled('div')`
+  border: 1px solid ${({ theme }) => theme.sharedTheme.basic.colorGrayLine};
+  border-radius: 4px;
+
+  & .ant-form-item.top-switch {
+    padding: 12px;
+
+    .ant-form-item-control-input-content {
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+
+  .audit-configuration-wrapper {
+    padding: 12px;
+    overflow: hidden;
+    background-color: ${({ theme }) => theme.sharedTheme.uiToken.colorBgLayout};
+  }
+
+  .audit-fields-wrapper {
+    background-color: ${({ theme }) => theme.sharedTheme.uiToken.colorBgBase};
+    padding: 12px;
+    margin-bottom: 12px;
+    border-radius: 4px;
+
+    .ant-form-item:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .workbench-audit-fields {
+    background-color: ${({ theme }) => theme.sharedTheme.uiToken.colorBgBase};
+    border-radius: 4px;
+
+    .workbench-audit-content {
+      padding: 12px 12px 12px 24px;
+    }
+  }
+
+  & .ant-form-item.no-margin-field {
+    margin-bottom: 0;
+  }
+`;
