d55b83d3fe863909359fb5574bd0c2b8fd296c26
zzyangh <799463087@qq.com>
2025-11-11 17:01:19 +0800

    [feature](SqlOptimization): Adjust result page and add high-precision search form items

--- packages/sqle/src/page/SqlOptimization/Result/components/LeftContent.tsx
+++ packages/sqle/src/page/SqlOptimization/Result/components/LeftContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Space, Col } from 'antd';
-import { BasicButton } from '@actiontech/dms-kit';
+import { Space, Col, Alert, Typography } from 'antd';
+import { BasicButton, EmptyBox } from '@actiontech/dms-kit';
 import { SQLRenderer } from '@actiontech/shared';
 import {
   TransferFilled,
@@ -13,6 +13,12 @@ import CopyButton from './CopyButton';
 import QueryPlanFlow from './QueryPlanFlow';
 import { IQueryPlanNode } from '@actiontech/shared/lib/api/sqle/service/common';
 import { OptimizationSQLDetailStatusEnum } from '@actiontech/shared/lib/api/sqle/service/common.enum';
+import BestPerformanceIcon from './BestPerformanceIcon';
+import {
+  SqlOptimizationCardSubTitleStyleWrapper,
+  SqlOptimizationOtherAdviceTitleStyleWrapper
+} from './style';
+import SqlDiffView from './SqlDiffView';
 
 export interface LeftContentProps {
   optimizedSql?: string;
@@ -25,6 +31,10 @@ export interface LeftContentProps {
   onExpandQueryPlan: () => void;
   isVerticalLayout?: boolean;
   optimizationStatus?: OptimizationSQLDetailStatusEnum;
+  isBestPerformance: boolean;
+  hasAdvice?: boolean;
+  otherAdvice?: string;
+  originalSql?: string;
 }
 
 const LeftContent: React.FC<LeftContentProps> = ({
@@ -37,70 +47,161 @@ const LeftContent: React.FC<LeftContentProps> = ({
   onViewQueryPlanDiff,
   onExpandQueryPlan,
   isVerticalLayout = false,
-  optimizationStatus
+  optimizationStatus,
+  isBestPerformance,
+  hasAdvice,
+  otherAdvice,
+  originalSql
 }) => {
   const { t } = useTranslation();
+
+  const showAdvice = (hasAdvice && !!otherAdvice) || !advisedIndex;
+
+  const isOptimizationFinished =
+    optimizationStatus === OptimizationSQLDetailStatusEnum.finish;
 
   return (
     <Col span={isVerticalLayout ? 24 : 16}>
       <Space direction="vertical" size={16}>
         {/* SQL优化结果 */}
+        <EmptyBox if={isBestPerformance && isOptimizationFinished}>
+          <Alert
+            showIcon
+            type="info"
+            message={t('sqlOptimization.result.bestPerformanceSqlTips')}
+            icon={<BestPerformanceIcon />}
+          />
+        </EmptyBox>
         <SqlOptimizationCard
-          title={t('sqlOptimization.result.newOptimizedQuery')}
+          title={
+            isBestPerformance
+              ? t('sqlOptimization.result.originalQuery')
+              : t('sqlOptimization.result.newOptimizedQuery')
+          }
           extra={
             <Space>
-              <BasicButton
-                icon={<TransferFilled />}
-                size="small"
-                onClick={onViewOverallDiff}
+              <EmptyBox
+                if={isBestPerformance}
+                defaultNode={
+                  <BasicButton
+                    icon={<TransferFilled />}
+                    size="small"
+                    onClick={onViewOverallDiff}
+                  >
+                    {t('sqlOptimization.result.viewDifference')}
+                  </BasicButton>
+                }
               >
-                {t('sqlOptimization.result.viewDifference')}
-              </BasicButton>
-              <CopyButton content={optimizedSql} />
+                <CopyButton content={optimizedSql} />
+              </EmptyBox>
             </Space>
           }
           isEmpty={!optimizedSql}
           errorMessage={errorMessage}
           optimizationStatus={optimizationStatus}
         >
-          <SQLRenderer wordWrap showLineNumbers sql={optimizedSql} />
+          <EmptyBox
+            if={isBestPerformance}
+            defaultNode={
+              <SqlDiffView
+                originalSql={originalSql ?? ''}
+                optimizedSql={optimizedSql ?? ''}
+                originTitle={t('sqlOptimization.result.original')}
+                optimizedTitle={t('sqlOptimization.result.finalOptimized')}
+              />
+            }
+          >
+            <SQLRenderer wordWrap showLineNumbers sql={optimizedSql} />
+          </EmptyBox>
         </SqlOptimizationCard>
 
         {/* 索引优化建议 */}
-        <SqlOptimizationCard
-          title={t('sqlOptimization.result.indexOptimizationAdvice')}
-          extra={
-            <Space>
-              <BasicButton
-                icon={<MagnifierFilled />}
-                size="small"
-                onClick={onViewTableStructure}
-              >
-                {t('sqlOptimization.result.viewTableStructure')}
-              </BasicButton>
-              <CopyButton content={advisedIndex} />
-            </Space>
+        <EmptyBox
+          if={showAdvice && isOptimizationFinished}
+          defaultNode={
+            <SqlOptimizationCard
+              title={
+                <Space direction="vertical" size={2}>
+                  {t('sqlOptimization.result.indexOptimizationAdvice')}
+                  <EmptyBox
+                    if={isBestPerformance}
+                    defaultNode={
+                      <SqlOptimizationCardSubTitleStyleWrapper type="secondary">
+                        {t(
+                          'sqlOptimization.result.indexOptimizationAdviceTips'
+                        )}
+                      </SqlOptimizationCardSubTitleStyleWrapper>
+                    }
+                  >
+                    <SqlOptimizationCardSubTitleStyleWrapper type="secondary">
+                      {t('sqlOptimization.result.indexOriginalAdviceTips')}
+                    </SqlOptimizationCardSubTitleStyleWrapper>
+                  </EmptyBox>
+                </Space>
+              }
+              extra={
+                <Space>
+                  <BasicButton
+                    icon={<MagnifierFilled />}
+                    size="small"
+                    onClick={onViewTableStructure}
+                  >
+                    {t('sqlOptimization.result.viewTableStructure')}
+                  </BasicButton>
+                  <CopyButton content={advisedIndex} />
+                </Space>
+              }
+              isEmpty={!advisedIndex}
+              errorMessage={errorMessage}
+              className="execution-plan-flow-card"
+              optimizationStatus={optimizationStatus}
+            >
+              <SQLRenderer wordWrap showLineNumbers sql={advisedIndex} />
+            </SqlOptimizationCard>
           }
-          isEmpty={!advisedIndex}
-          errorMessage={errorMessage}
-          className="execution-plan-flow-card"
-          optimizationStatus={optimizationStatus}
         >
-          <SQLRenderer wordWrap showLineNumbers sql={advisedIndex} />
-        </SqlOptimizationCard>
+          <SqlOptimizationCard title="">
+            <EmptyBox
+              if={hasAdvice && !!otherAdvice}
+              defaultNode={
+                <Space>
+                  <BestPerformanceIcon />
+                  <span>
+                    {isBestPerformance
+                      ? t('sqlOptimization.result.bestIndexUsedForOriginalSql')
+                      : t(
+                          'sqlOptimization.result.bestIndexUsedForOptimizedSql'
+                        )}
+                  </span>
+                </Space>
+              }
+            >
+              <SqlOptimizationOtherAdviceTitleStyleWrapper direction="vertical">
+                <span className="advice-title">
+                  {t('sqlOptimization.result.cannotOptimizeByIndexTips')}
+                </span>
+                <Typography.Text type="secondary">
+                  {otherAdvice}
+                </Typography.Text>
+              </SqlOptimizationOtherAdviceTitleStyleWrapper>
+            </EmptyBox>
+          </SqlOptimizationCard>
+        </EmptyBox>
 
         {/* 优化后的执行计划 */}
         <SqlOptimizationCard
           title={t('sqlOptimization.result.optimizedExecutionPlan')}
           extra={
             <Space>
-              <BasicButton
-                icon={<TransferFilled />}
-                size="small"
-                onClick={onViewQueryPlanDiff}
-              >
-                {t('sqlOptimization.result.viewDifference')}
-              </BasicButton>
+              <EmptyBox if={!isBestPerformance}>
+                <BasicButton
+                  icon={<TransferFilled />}
+                  size="small"
+                  onClick={onViewQueryPlanDiff}
+                >
+                  {t('sqlOptimization.result.viewDifference')}
+                </BasicButton>
+              </EmptyBox>
               <BasicButton
                 icon={<FullScreenOutlined />}
                 size="small"
