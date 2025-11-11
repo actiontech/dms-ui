b218cd8d79b446a6fe34ab848573b3b11c5df633
zzyangh <799463087@qq.com>
2025-11-11 17:03:57 +0800

    [test]: Update unit tests

--- packages/sqle/src/page/SqlOptimization/Result/components/__tests__/RightContent.test.tsx
+++ packages/sqle/src/page/SqlOptimization/Result/components/__tests__/RightContent.test.tsx
@@ -5,6 +5,7 @@ import {
   mockUseCurrentUser
 } from '@actiontech/shared/lib/testUtil';
 import { OptimizationSQLDetailStatusEnum } from '@actiontech/shared/lib/api/sqle/service/common.enum';
+import { screen } from '@testing-library/react';
 
 describe('RightContent', () => {
   const mockProps = {
@@ -17,7 +18,7 @@ describe('RightContent', () => {
         }
       ]
     },
-    optimiationStatus: OptimizationSQLDetailStatusEnum.finish,
+    optimizationStatus: OptimizationSQLDetailStatusEnum.finish,
     optimizeSteps: [
       {
         chat_id: 'chat_1',
@@ -78,5 +79,25 @@ describe('RightContent', () => {
     };
     const { baseElement } = superRender(<RightContent {...errorProps} />);
     expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should render with optimizeSteps is empty', () => {
+    const { baseElement } = superRender(
+      <RightContent {...mockProps} optimizeSteps={[]} />
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should render with totalAnalysis detail is empty', () => {
+    const { baseElement } = superRender(
+      <RightContent
+        {...mockProps}
+        totalAnalysis={{ ...mockProps.totalAnalysis, detail: [] }}
+      />
+    );
+    expect(baseElement).toMatchSnapshot();
+    expect(
+      screen.getByText('恭喜！ 您的SQL查询配置已经非常优秀，无需进一步优化。')
+    ).toBeInTheDocument();
   });
 });
