a05c3df0c46f6792664d0da979e87b5cf3e6d2ff
zzyangh <799463087@qq.com>
2025-11-24 13:17:49 +0800

    [docs](BasicRangePicker): Adjust docs

--- packages/dms-kit/src/components/BasicRangePicker/demos/basic.tsx
+++ packages/dms-kit/src/components/BasicRangePicker/demos/basic.tsx
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { BasicRangePicker, ConfigProvider } from '@actiontech/dms-kit';
+import { Space, Typography } from 'antd';
 import type { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
+import { CalendarOutlined } from '@actiontech/icons';
 
+const { Text } = Typography;
+
+/**
+ * 基础用法
+ * - 简单的日期范围选择
+ * - 支持不同尺寸和禁用状态
+ * - 支持限制日期范围
+ * - 支持前缀图标和年份/月份快速切换
+ */
 const BasicRangePickerBasicDemo: React.FC = () => {
   const [dateRange, setDateRange] = useState<
     [Dayjs | null, Dayjs | null] | null
@@ -15,18 +27,83 @@ const BasicRangePickerBasicDemo: React.FC = () => {
     if (dates && dates[0] && dates[1]) {
       console.log('开始日期:', dates[0].format('YYYY-MM-DD'));
       console.log('结束日期:', dates[1].format('YYYY-MM-DD'));
-      console.log('开始日期字符串:', dateStrings[0]);
-      console.log('结束日期字符串:', dateStrings[1]);
     }
+  };
+
+  // 禁用今天之前的日期
+  const disabledDate = (current: Dayjs) => {
+    return current && current < dayjs().startOf('day');
   };
 
   return (
     <ConfigProvider>
-      <BasicRangePicker
-        value={dateRange}
-        onChange={handleRangeChange}
-        placeholder={['开始日期', '结束日期']}
-      />
+      <Space direction="vertical" size="middle">
+        <Space>
+          <Text>基础用法：</Text>
+          <BasicRangePicker
+            value={dateRange}
+            onChange={handleRangeChange}
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>小尺寸：</Text>
+          <BasicRangePicker
+            size="small"
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>大尺寸：</Text>
+          <BasicRangePicker
+            size="large"
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>带默认值：</Text>
+          <BasicRangePicker
+            defaultValue={[dayjs().subtract(7, 'day'), dayjs()]}
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>禁用状态：</Text>
+          <BasicRangePicker
+            disabled
+            defaultValue={[dayjs().subtract(7, 'day'), dayjs()]}
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>限制日期（今天之后）：</Text>
+          <BasicRangePicker
+            disabledDate={disabledDate}
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>带前缀图标：</Text>
+          <BasicRangePicker
+            prefix={<CalendarOutlined />}
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+
+        <Space>
+          <Text>显示年份快速切换按钮：</Text>
+          <BasicRangePicker
+            hideSuperIcon={false}
+            placeholder={['开始日期', '结束日期']}
+          />
+        </Space>
+      </Space>
     </ConfigProvider>
   );
 };
