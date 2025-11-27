3d08bf57ec80a6f425fbeca0ff9fac2d41c08cc5
zzyangh <799463087@qq.com>
2025-11-27 15:55:03 +0800

    [docs](BasicButton): Adjust docs

--- packages/dms-kit/src/components/BasicButton/demo/icon.tsx
+++ packages/dms-kit/src/components/BasicButton/demo/icon.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import { BasicButton, ConfigProvider } from '@actiontech/dms-kit';
-import { PlusOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Space, Typography } from 'antd';
+import {
+  PlusOutlined,
+  SearchOutlined,
+  CheckCircleOutlined,
+  CloseCircleOutlined
+} from '@actiontech/icons';
 
+const { Title } = Typography;
+
+/**
+ * 图标按钮
+ * - 普通图标按钮
+ * - 无边框图标按钮（noBorderIcon）
+ * - 带文本的图标按钮
+ */
 const IconButtonDemo = () => {
   return (
     <ConfigProvider>
-      <div style={{ display: 'flex', gap: 8 }}>
-        <BasicButton icon={<PlusOutlined />} />
-        <BasicButton icon={<EditOutlined />} />
-        <BasicButton icon={<DeleteOutlined />} />
-        <BasicButton noBorderIcon icon={<PlusOutlined />} />
-      </div>
+      <Space direction="vertical" size="large" style={{ width: '100%' }}>
+        <div>
+          <Title level={5}>普通图标按钮</Title>
+          <Space wrap>
+            <BasicButton icon={<PlusOutlined color="white" />} type="primary" />
+            <BasicButton icon={<SearchOutlined />} />
+            <BasicButton icon={<CloseCircleOutlined />} danger />
+            <BasicButton icon={<CheckCircleOutlined />} type="dashed" />
+          </Space>
+        </div>
+
+        <div>
+          <Title level={5}>无边框图标按钮</Title>
+          <Space wrap>
+            <BasicButton noBorderIcon icon={<PlusOutlined />} />
+            <BasicButton noBorderIcon icon={<SearchOutlined />} />
+            <BasicButton noBorderIcon icon={<CloseCircleOutlined />} />
+            <BasicButton noBorderIcon icon={<CheckCircleOutlined />} />
+          </Space>
+        </div>
+
+        <div>
+          <Title level={5}>带文本的图标按钮</Title>
+          <Space wrap>
+            <BasicButton icon={<PlusOutlined color="white" />} type="primary">
+              新建
+            </BasicButton>
+            <BasicButton icon={<SearchOutlined />}>搜索</BasicButton>
+            <BasicButton icon={<CloseCircleOutlined />} danger>
+              删除
+            </BasicButton>
+            <BasicButton icon={<CheckCircleOutlined />}>确认</BasicButton>
+          </Space>
+        </div>
+      </Space>
     </ConfigProvider>
   );
 };
