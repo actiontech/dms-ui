4834970f2cab93f5bc1422a5b351141fbd17870c
zzyangh <799463087@qq.com>
2025-12-09 13:33:57 +0800

    [feature](DataExportManagement): Adjust display of reminder

---
+++ packages/base/src/page/DataExportManagement/Create/components/SubmitResult/style.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material/styles';
+import { Alert } from 'antd';
+
+export const SubmitResultAlertStyleWrapper = styled(Alert)`
+  &.ant-alert.ant-alert-info .ant-alert-content {
+    text-align: left;
+  }
+`;
