480b843a2324cff7414569fabdaa6b50ce8bb342
lizhensheng <lizhensheng@actionsky.com>
2026-04-03 15:09:47 +0800

    [chore]: add Account Panel in sqle

---
+++ packages/sqle/src/page/GlobalDashboard/components/AccountPanel/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * Do not modify this file in the dms-ui repository
+ */
+
+type AccountPanelProps = {
+  projectId?: string;
+  instanceId?: string;
+  refreshSignal?: number;
+  isAdmin?: boolean;
+};
+
+const AccountPanel: React.FC<AccountPanelProps> = ({}) => {
+  return null;
+};
+
+export default AccountPanel;
