70c24ce4512dbc28c0823745cc31dd1a1acea979
actiontech-zihan <yangzhongjiao@actionsky.com>
2026-07-21 10:15:02 +0000

    test: update create-form snapshots and mock template tips hook
    Keep enzyme BaseInfoFormItem tests independent from Redux by mocking
    useWorkflowTemplateTips after multi-template selection was added.

--- packages/sqle/src/page/SqlExecWorkflow/Create/components/FormStep/BaseInfoForm/__tests__/BaseInfoFormItem.test.tsx
+++ packages/sqle/src/page/SqlExecWorkflow/Create/components/FormStep/BaseInfoForm/__tests__/BaseInfoFormItem.test.tsx
@@ -2,6 +2,14 @@ import { shallow } from 'enzyme';
 import BaseInfoFormItem from '../BaseInfoFormItem';
 import toJson from 'enzyme-to-json';
 
+jest.mock('../../../../../../../hooks/useWorkflowTemplateTips', () => () => ({
+  templateList: [],
+  templateOptions: [],
+  defaultTemplateId: undefined,
+  loading: false,
+  refresh: jest.fn()
+}));
+
 describe('test BaseInfoFormItem', () => {
   it('should match snapshot', () => {
     const wrapper = shallow(<BaseInfoFormItem />);
